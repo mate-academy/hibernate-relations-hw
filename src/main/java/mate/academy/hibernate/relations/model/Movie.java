c04245e2744a1b53f8d2f32df98528f54beeca2c
Dalai <semenov.dalay@gmail.com>
2021-05-24 19:13:42 +0300

    added solution

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,11 +1,22 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlInlineBinaryData;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "movies")
 public class Movie implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "actors_movies",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id"))
     private List<Actor> actors;
 
     public Movie() {
