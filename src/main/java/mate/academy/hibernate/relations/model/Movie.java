1ea70a45d327ddb3d666e84345f5503c9eaa22a7
Dalai <semenov.dalay@gmail.com>
2021-05-24 19:17:01 +0300

    fixed checkstyle

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.model;
 
-import javax.persistence.*;
-import javax.xml.bind.annotation.XmlInlineBinaryData;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "movies")
