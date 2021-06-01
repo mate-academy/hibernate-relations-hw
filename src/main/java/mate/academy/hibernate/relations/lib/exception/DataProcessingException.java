f19d5653d3a5df2cdf6512b8f56226bba0cbb73c
Maksim <max.freedman.main@gmail.com>
2021-06-01 15:13:05 +0300

    created DataProcessingException

---
+++ src/main/java/mate/academy/hibernate/relations/lib/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.academy.hibernate.relations.lib.exception;
+
+public class DataProcessingException extends RuntimeException{
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
