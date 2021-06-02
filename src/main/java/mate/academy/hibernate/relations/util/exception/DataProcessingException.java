c4f3571925f2a2adc65e0f08992ff79f6f831501
peps <pepsithedoge@gmail.com>
2021-06-02 16:18:30 +0300

    added solution

---
+++ src/main/java/mate/academy/hibernate/relations/util/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.academy.hibernate.relations.util.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
