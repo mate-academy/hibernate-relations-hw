b6698b17469bd5cda7a2ec5dab5413bdb98ad116
DeStinyShell <dmitriy4ever97@gmail.com>
2021-06-02 10:44:23 +0300

    Done Homework.

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,19 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = createSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory createSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return sessionFactory;
     }
 }
