8ab1665dcbf08e749f0d99c12271628c4746364a
Alina <vasylyshyn.alina@gmail.com>
2021-06-02 13:13:19 +0300

    checkstyle

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -4,15 +4,11 @@ import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
-import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
-    private static final AbstractDao abstractDao = new ActorDaoImpl(HibernateUtil
-            .getSessionFactory());
-
     public CountryDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
