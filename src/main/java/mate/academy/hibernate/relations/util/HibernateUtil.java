6d487c8cc0441d959994f48c62a832dc1ee0a261
Borys Skvortsov <Dhakmar@gmail.com>
2021-07-31 13:04:48 +0300

    Implemented all DAO layer;

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,21 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory SESSION_FACTORY = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration()
+                .configure()
+                .buildSessionFactory();
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return SESSION_FACTORY;
     }
 }
