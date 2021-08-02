c92b4c52297c6028bb71c3a878937e9011df6413
olesiasmitiukh <d.olleska@gmail.com>
2021-08-02 14:52:26 +0300

    checked some stylish mistakes

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -40,7 +40,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
         try (Session session = factory.openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
         } catch (Exception e) {
-            throw new DataProcessingException("Couldn't get movie from DB were id: " + id, e);
+            throw new DataProcessingException("Couldn't get movie from DB where id: " + id, e);
         }
     }
 }
