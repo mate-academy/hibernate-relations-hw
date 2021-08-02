7651f45d2dff7eacf2513c2ff13cadc3a8578842
sSlonIk <kryachok@gmail.com>
2021-08-02 16:23:03 +0300

    fixed exception

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -26,7 +26,7 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can`t country actor: " + country, e);
+            throw new DataProcessingException("Can`t add country: " + country, e);
         } finally {
             if (session != null) {
                 session.close();
