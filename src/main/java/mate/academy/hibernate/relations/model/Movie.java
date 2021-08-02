19f971d261e6185b8dcb07e4b6c89f6331689708
PashaTerletskyi <p.terletsky@gmail.com>
2021-08-02 14:36:36 +0300

    fixed checkstyle

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -22,7 +22,7 @@ public class Movie implements Cloneable {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "movies_actors",
             joinColumns = @JoinColumn(name = "movie_id"),
-            inverseJoinColumns = @JoinColumn(name = "actors_id"))
+            inverseJoinColumns = @JoinColumn(name = "actor_id"))
     private List<Actor> actors;
 
     public Movie() {
