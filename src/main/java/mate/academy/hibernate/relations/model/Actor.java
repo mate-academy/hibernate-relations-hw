a45e19aab6fd337b108d43b1d9e0c86e7cabff15
Nikitozin <nikitozin91@gmail.com>
2021-08-02 14:56:19 +0300

    Resolve_problems

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -5,8 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-@Entity(name = "actors")
+@Entity
+@Table(name = "actors")
 public class Actor implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
