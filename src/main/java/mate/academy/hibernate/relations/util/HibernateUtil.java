978fb3e297b2ec005a5b4ee8ff4d7ee199c3d43c
Basilius the Third <champfockes@gmail.com>
2021-08-02 16:05:08 +0300

    small fixes

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -4,10 +4,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory instance = initSessionFactory();
+    private static final SessionFactory instance = initSessionFactory();
 
     private HibernateUtil() {
-
     }
 
     private static SessionFactory initSessionFactory() {
