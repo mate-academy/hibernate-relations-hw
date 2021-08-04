ab3f4b25732dd98766b1ccef528cfb29a1762912
Vova Bilodon <vovabilodon@gmail.com>
2021-08-04 09:22:58 +0300

    added empty line to hibernate.cfg.xml; instance in HibernateUtil is now final; names of the entity tables have been changed;

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -4,7 +4,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory instance = initSessionFactory();
+    private static final SessionFactory instance = initSessionFactory();
+
+    private HibernateUtil() {
+    }
 
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
