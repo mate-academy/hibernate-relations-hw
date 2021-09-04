d9adef0ad192ffaa84e8633a91ec043758a9f038
Viacheslav Cheban <vcheb@eg.dk>
2021-09-04 16:43:27 +0300

    fix HibernateUtil

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,18 +1,26 @@
 package mate.academy.hibernate.relations.util;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import org.hibernate.cfg.Configuration;
 
 
 public class HibernateUtil {
+    //    public static SessionFactory getSessionFactory() {
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mate-academy");
+//        EntityManager entityManager = emf.createEntityManager();
+//        Session session = entityManager.unwrap(org.hibernate.Session.class);
+//        return session.getSessionFactory();
+//    }
+    //
+    private static SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("mate-academy");
-        EntityManager entityManager = emf.createEntityManager();
-        Session session = entityManager.unwrap(org.hibernate.Session.class);
-        return session.getSessionFactory();
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        }
+        return sessionFactory;
     }
 }
