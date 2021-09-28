03a0b0b6e7d9f5bc9897425bae6c69ec6f397505
Viktor Yukhno <pingvinomania@gmail.com>
2021-09-28 14:46:43 +0300

    fixed after mentor's review

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -26,7 +26,7 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't add actor: " + actor.getName() + " to DB", e);
+            throw new DataProcessingException("Can't add actor: " + actor + " to DB", e);
         } finally {
             if (session != null) {
                 session.close();
--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -26,7 +26,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't add movie: " + movie.getTitle() + " to DB", e);
+            throw new DataProcessingException("Can't add movie: " + movie + " to DB", e);
         } finally {
             if (session != null) {
                 session.close();
