fc54e75025e79e7be9723f134c1ad7cc7fcc0da9
Vladyslav Gural <vladgural@gmail.com>
2021-10-21 16:14:29 +0300

    removed my code

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,28 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.lib.Inject;
+import mate.academy.hibernate.relations.lib.Service;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
+@Service
 public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao;
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        Optional<Movie> optionalMovie = movieDao.get(id);
+        if (optionalMovie.isPresent()) {
+            return optionalMovie.get();
+        }
+        throw new RuntimeException("Don't exist Movie with id " + id + " in DB");
     }
 }
