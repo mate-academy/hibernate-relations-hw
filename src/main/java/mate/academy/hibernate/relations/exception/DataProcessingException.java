d4673d1178575de0ce0b66c514b33ef1b47e89cc
Kristina Voytikh <61868920+kvoytikh@users.noreply.github.com>
2021-10-25 14:05:45 +0300

    hibernate_relations prj
    hibernate_relations prj

---
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -0,0 +1,11 @@
+package mate.academy.hibernate.relations.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public DataProcessingException(String message) {
+        super(message);
+    }
+}
