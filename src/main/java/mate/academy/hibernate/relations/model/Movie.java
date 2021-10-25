456af03fe83f47f206dafd880eb2d586599b4b5f
Kristina Voytikh <61868920+kvoytikh@users.noreply.github.com>
2021-10-25 18:43:21 +0300

    Add files via upload

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -19,6 +19,7 @@ public class Movie implements Cloneable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "movies_actors",
             joinColumns = @JoinColumn(name = "movie_id"),
