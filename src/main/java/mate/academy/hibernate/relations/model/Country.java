35b71d02e0b4e4e0089a09f40a5995a634d7319a
SolnalarmJulia <solnalarm@meta.ua>
2021-10-25 13:59:41 +0300

    relations

--- src/main/java/mate/academy/hibernate/relations/model/Country.java
+++ src/main/java/mate/academy/hibernate/relations/model/Country.java
@@ -1,6 +1,17 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity(name = "country")
+@Table(name = "countries")
 public class Country implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
@@ -25,6 +36,20 @@ public class Country implements Cloneable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(id, country.id) &&
+                Objects.equals(name, country.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     @Override
--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,23 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    public static SessionFactory getSessionFactory() {
-        return null;
+    private static SessionFactory instance = initSessionFactory();
+
+    public HibernateUtil() {
     }
+
+    public HibernateUtil(SessionFactory sessionFactory) {
+    }
+
+    public static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return instance;
+    }
+
 }
