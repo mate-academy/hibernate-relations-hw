4a785835524daae8c7cacf486d7a57abfe59b903
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-12-13 20:53:15 +0200

    made it work finally

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -1,9 +1,13 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import exception.DataProcessingException;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public CountryDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,36 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Country add(Country country) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(country);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new DataProcessingException(
+                        "Can't commit transaction. Transaction is rolled back", e);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return country;
     }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        Optional<Country> country;
+        try (Session session = factory.openSession()) {
+            country = Optional.ofNullable(session.get(Country.class, id));
+        } catch (HibernateException e) {
+            throw new DataProcessingException(
+                    "Can't get movie with id = " + id + "from DB", e);
+        }
+        return country;
     }
 }
--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -1,9 +1,13 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import exception.DataProcessingException;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MovieDaoImpl extends AbstractDao implements MovieDao {
     public MovieDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,36 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
 
     @Override
     public Movie add(Movie movie) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new DataProcessingException(
+                        "Can't commit transaction. Transaction is rolled back", e);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return null;
+        Optional<Movie> movie;
+        try (Session session = factory.openSession()) {
+            movie = Optional.ofNullable(session.get(Movie.class, id));
+        } catch (HibernateException e) {
+            throw new DataProcessingException(
+                    "Can't get movie with id = " + id + "from DB", e);
+        }
+        return movie;
     }
 }
