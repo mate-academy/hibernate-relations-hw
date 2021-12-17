3067272e08eeb7b1fbb8cc859ea3556d5923b650
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2021-12-17 16:29:18 +0200

    Added first student review suggestions

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -19,6 +19,6 @@ public class ActorServiceImpl implements ActorService {
     @Override
     public Actor get(Long id) {
         return actorDao.get(id).orElseThrow(() ->
-                new RuntimeException("Cant get actor from DB by id " + id));
+                new RuntimeException("Can't get actor from DB by id " + id));
     }
 }
