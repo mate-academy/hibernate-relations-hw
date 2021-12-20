dd68c6ea1ceeae413f4eb77a3fc67c2448552ec9
Anton Volskyi <antonvolskyi1234@gmail.com>
2021-12-20 17:59:02 +0200

    common mistakes fix

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -27,7 +27,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can`t write Actor to DB: " + movie, e);
+            throw new DataProcessingException("Can`t write Movie to DB: " + movie, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -40,7 +40,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
         try (Session session = factory.openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
         } catch (Exception e) {
-            throw new DataProcessingException("Can`t get Actor with ID: " + id, e);
+            throw new DataProcessingException("Can`t get Movie with ID: " + id, e);
         }
     }
 }
