5c5256ef67e629726af02fbdda38e1dae304c361
farfardesu <savavlad003@gmail.com>
2021-12-22 11:38:23 +0200

    suggested changes were implemented

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -41,7 +41,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
             Movie movie = session.get(Movie.class, id);
             return Optional.ofNullable(movie);
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get Country with id: " + id, e);
+            throw new DataProcessingException("Can't get Movie with id: " + id, e);
         }
     }
 }
