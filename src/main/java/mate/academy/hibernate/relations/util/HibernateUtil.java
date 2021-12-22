e2079974b0ef2ee1d9d63ef052bbe0d04aeab170
Volodimir <vova.stepanenko2020@gmail.com>
2021-12-22 12:49:58 +0200

    private default HibernateUtil constructor

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -6,6 +6,8 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory instance = initSessionFactory();
 
+    private HibernateUtil() {}
+
     public static SessionFactory getSessionFactory() {
         return instance;
     }
