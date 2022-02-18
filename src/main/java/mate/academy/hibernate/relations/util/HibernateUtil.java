784b0e2e3f1a0fac27338796d416a50e6135dff4
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-18 21:06:11 +0200

    Implement HibernateUtil

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,20 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory SESSION_FACTORY;
+
+    static {
+        SESSION_FACTORY = new Configuration()
+                .configure().
+                buildSessionFactory();
+    }
+
+    private HibernateUtil() { }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return SESSION_FACTORY;
     }
 }
