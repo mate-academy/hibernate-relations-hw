36382874e4b8e7fedf5d26304fc51e6e95fa8bbf
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-15 12:43:35 +0200

    implemented CountryDaoImpl, CountryServiceImpl, checked it in Main and added DataProcessingException

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -1,25 +1,29 @@
 package mate.academy.hibernate.relations;
 
 import java.util.List;
+
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.ActorService;
 import mate.academy.hibernate.relations.service.CountryService;
 import mate.academy.hibernate.relations.service.MovieService;
+import mate.academy.hibernate.relations.service.impl.CountryServiceImpl;
 import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        // use this session factory when you will initialize service instances
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
         Country usa = new Country("USA");
-        CountryService countryService = null; // TODO: initialize this instance
+        CountryDao countryDao = new CountryDaoImpl(sessionFactory);
+        CountryService countryService = new CountryServiceImpl(countryDao);
         countryService.add(usa);
 
-        Actor vinDiesel = new Actor("Vin Diesel");
+        /*Actor vinDiesel = new Actor("Vin Diesel");
         vinDiesel.setCountry(usa);
         ActorService actorService = null; // TODO: initialize this instance
         actorService.add(vinDiesel);
@@ -28,6 +32,6 @@ public class Main {
         fastAndFurious.setActors(List.of(vinDiesel));
         MovieService movieService = null; // TODO: initialize this instance
         movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
+        System.out.println(movieService.get(fastAndFurious.getId()));*/
     }
 }
