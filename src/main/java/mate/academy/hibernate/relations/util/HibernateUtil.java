850c13740e19eff1efcfbb6cba850715f0bff396
nastyaprokhorenko <nastyaprokhorenko527@gmail.com>
2022-03-23 18:54:04 +0200

    default HibernateUtil constructor was added

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -6,6 +6,10 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static SessionFactory instance = initSessionFactory();
 
+    private HibernateUtil() {
+
+    }
+
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
