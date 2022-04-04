b838841bf2c6a316d3c439797df0792360bdb933
Viktor Stadnik <stadhik.victor@gmail.com>
2022-04-04 19:03:38 +0300

    corrected comments in block "catch"

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -40,7 +40,7 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
         try (Session session = factory.openSession();) {
             return Optional.ofNullable(session.get(Country.class, id));
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get id: " + id, e);
+            throw new DataProcessingException("Can't get country by id: " + id, e);
         }
     }
 }
