4efd7970f475b6fcafdef2f2eefdc7b22981207c
Leonid <rommel@ukr.net>
2022-04-12 11:23:49 +0300

    Private constructor added to HibernateUtil

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -4,6 +4,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
