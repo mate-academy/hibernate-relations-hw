be7f5e6b34b09b7c7f64f9de44552c9525b6aa71
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-04-17 18:35:53 +0300

    Implemented relations between entities.

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,19 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory instance = innitSessionFactory();
+
+    public HibernateUtil() {
+    }
+
+    private static SessionFactory innitSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
     }
 }
