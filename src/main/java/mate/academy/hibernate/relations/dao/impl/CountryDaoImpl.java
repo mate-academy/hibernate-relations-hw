33f1f493e4bb37a925bd8962b7f73463a6da1dd5
Mark Shapka <mark.shapka1@gmail.com>
2022-05-20 18:28:10 +0300

    - changed implementation get() methods into classes *DaoImpl and exception message - added empty line into hibernate.cfg.xml file - changed annotation @ManyToMany in the movie class - changed annotation @ManyToOne in the actor class

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -37,16 +37,10 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Optional<Country> get(Long id) {
-        Session session = null;
-        try {
-            session = factory.openSession();
+        try (Session session = factory.openSession()) {
             return Optional.ofNullable(session.get(Country.class, id));
         } catch (Exception e) {
-            throw new DataProcessingException("Can't find country by Id: " + id, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+            throw new DataProcessingException("Can't get country by Id: " + id, e);
         }
     }
 }
