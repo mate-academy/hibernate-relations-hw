3d5983a905e918b8dada901052c8697adad9afa6
Andrii Fabersky <Andriifabersky@gmail.com>
2022-05-31 16:55:43 +0300

    I did my homework

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -1,33 +1,52 @@
 package mate.academy.hibernate.relations;
 
 import java.util.List;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
+import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.ActorService;
 import mate.academy.hibernate.relations.service.CountryService;
 import mate.academy.hibernate.relations.service.MovieService;
+import mate.academy.hibernate.relations.service.impl.ActorServiceImpl;
+import mate.academy.hibernate.relations.service.impl.CountryServiceImpl;
+import mate.academy.hibernate.relations.service.impl.MovieServiceImpl;
 import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        // use this session factory when you will initialize service instances
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
         Country usa = new Country("USA");
-        CountryService countryService = null; // TODO: initialize this instance
+        CountryService countryService = new CountryServiceImpl(new CountryDaoImpl(sessionFactory));
         countryService.add(usa);
+
+        Country ua = new Country("Ukraine");
+        countryService.add(ua);
 
         Actor vinDiesel = new Actor("Vin Diesel");
         vinDiesel.setCountry(usa);
-        ActorService actorService = null; // TODO: initialize this instance
+        Actor robertDowneyJr = new Actor("Robert Downey Jr.");
+        robertDowneyJr.setCountry(ua);
+        Actor johnnyDepp = new Actor("Johnny Depp");
+        johnnyDepp.setCountry(ua);
+        ActorService actorService = new ActorServiceImpl(new ActorDaoImpl(sessionFactory));
         actorService.add(vinDiesel);
+        actorService.add(robertDowneyJr);
+        actorService.add(johnnyDepp);
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setActors(List.of(vinDiesel));
-        MovieService movieService = null; // TODO: initialize this instance
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
+        Movie ironMan = new Movie("Iron Man");
+        Movie piratesOfTheCaribbean = new Movie("Pirates of the Caribbean");
+        ironMan.setActors(List.of(robertDowneyJr));
+        piratesOfTheCaribbean.setActors(List.of(johnnyDepp, vinDiesel));
+
+        MovieService movieService = new MovieServiceImpl(new MovieDaoImpl(sessionFactory));
+        movieService.add(ironMan);
+        movieService.add(piratesOfTheCaribbean);
+        System.out.println(movieService.get(ironMan.getId()));
+        System.out.println(movieService.get(piratesOfTheCaribbean.getId()));
     }
 }
