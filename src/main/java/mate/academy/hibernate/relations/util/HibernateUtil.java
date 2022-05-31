361d105c2bd50aa44f6342b0869d98a54faf262a
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-05-31 14:29:48 +0300

    first commit

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,20 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory instance = initFactory();
+
+    private HibernateUtil() {
+
+    }
+
+    private static SessionFactory initFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
     }
 }
