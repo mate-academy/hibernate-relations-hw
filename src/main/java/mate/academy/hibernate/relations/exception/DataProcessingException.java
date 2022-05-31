681fd569e89296f230bceef1b75add86c68f3daf
Oleksander <ghjwtyrj25@gmail.com>
2022-05-31 16:14:19 +0300

    ater deleting constructor from exception

--- src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -1,10 +1,6 @@
 package mate.academy.hibernate.relations.exception;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message) {
-        super(message);
-    }
-
     public DataProcessingException(String message, Throwable cause) {
         super(message, cause);
     }
