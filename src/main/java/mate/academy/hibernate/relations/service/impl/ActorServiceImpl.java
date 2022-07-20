b360c8b442b22df148322b3a18e3041f494d0626
Dima Podakin <calombo.podakin@ukr.net>
2022-07-20 22:03:15 +0300

    added message excaption to the orElseThrow

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
@@ -18,6 +19,7 @@ public class ActorServiceImpl implements ActorService {
 
     @Override
     public Actor get(Long id) {
-        return actorDao.get(id).orElseThrow();
+        return actorDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("Cannot get actor by this id: " + id));
     }
 }
