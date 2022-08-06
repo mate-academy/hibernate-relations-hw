46d73f8446dce61de5f85f736ddc146b0210db88
yaroslav <morozov.yrv@gmail.com>
2022-08-06 07:38:27 +0300

    checkstyle

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -5,12 +5,15 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory = initSessionFactory();
+
     private HibernateUtil() {
 
     }
+
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
+
     public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
