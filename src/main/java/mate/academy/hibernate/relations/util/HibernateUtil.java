55925a3d57f160a8b773b6cd6a23740b25deffaf
Oleksii Mospan <alexey.mospan@gmail.com>
2022-08-09 03:37:50 +0300

    Added implementations to HibernateUtil

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,10 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     public static SessionFactory getSessionFactory() {
-        return null;
+        return new Configuration().configure().buildSessionFactory();
     }
 }
