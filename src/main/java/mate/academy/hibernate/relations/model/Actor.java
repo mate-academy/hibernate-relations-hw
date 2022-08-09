c5ded53b293399cd6bd5f57cebfb023864d11035
Vitalii Smahlenko <smaglenko@gmail.com>
2022-08-09 08:39:23 +0300

    Refactoring the app after a mentor's note

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 @Table(name = "actors")
 public class Actor implements Cloneable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @OneToOne
