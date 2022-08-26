1e31977d4fa4a9e13c0dc6b8e216a1b0667c3a88
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-08-26 22:23:19 +0300

    Main,Actor remove empty lines

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -15,7 +15,6 @@ public class Actor implements Cloneable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
     @ManyToOne(fetch = FetchType.EAGER)
     private Country country;
 
