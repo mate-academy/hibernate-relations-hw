e82b285f264280eddca8ab77ea239c803f95c2c8
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-09-03 18:09:36 +0300

    Make Main class work

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -1,6 +1,7 @@
 package mate.academy.hibernate.relations;
 
 import java.util.List;
+import mate.academy.hibernate.relations.lib.Injector;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.model.Movie;
@@ -11,22 +12,28 @@ import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
 public class Main {
-    public static void main(String[] args) {
-        // use this session factory when you will initialize service instances
-        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private static final Injector injector =
+            Injector.getInstance("mate.academy.hibernate.relations");
+    private static final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
+    public static void main(String[] args) {
         Country usa = new Country("USA");
-        CountryService countryService = null; // TODO: initialize this instance
+        CountryService countryService =
+                (CountryService) injector.getInstance(CountryService.class, sessionFactory);
         countryService.add(usa);
+        System.out.println(countryService.get(usa.getId()));
 
         Actor vinDiesel = new Actor("Vin Diesel");
         vinDiesel.setCountry(usa);
-        ActorService actorService = null; // TODO: initialize this instance
+        ActorService actorService =
+                (ActorService) injector.getInstance(ActorService.class, sessionFactory);
         actorService.add(vinDiesel);
+        System.out.println(actorService.get(vinDiesel.getId()));
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setActors(List.of(vinDiesel));
-        MovieService movieService = null; // TODO: initialize this instance
+        MovieService movieService =
+                (MovieService) injector.getInstance(MovieService.class, sessionFactory);
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
     }
