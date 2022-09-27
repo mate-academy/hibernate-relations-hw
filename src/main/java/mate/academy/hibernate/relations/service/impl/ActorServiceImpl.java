29e40367f66c6342e65522ff74ff8673581ca2b2
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-09-27 22:50:05 +0300

    1) ElseThrow added instead get 2) Empty line added

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
@@ -19,6 +20,7 @@ public class ActorServiceImpl implements ActorService {
 
     @Override
     public Actor get(Long id) {
-        return actorDao.get(id).get();
+        return actorDao.get(id).orElseThrow(() ->
+                new NoSuchElementException("Cant get an actor by id: " + id));
     }
 }
