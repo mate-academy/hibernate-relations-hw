135dd9f272ccb70219c9a0735e3686aa3357de77
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-09-27 18:53:03 +0300

    remake service implementations

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,17 +1,15 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.ActorDao;
-import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
-import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
     private ActorDao dao;
     
-    public ActorServiceImpl(SessionFactory sessionFactory) {
-        this.dao = new ActorDaoImpl(sessionFactory);
+    public ActorServiceImpl(ActorDao dao) {
+        this.dao = dao;
     }
     
     @Override
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,17 +1,15 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.MovieDao;
-import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
-import org.hibernate.SessionFactory;
 
 public class MovieServiceImpl implements MovieService {
     private MovieDao dao;
     
-    public MovieServiceImpl(SessionFactory sessionFactory) {
-        this.dao = new MovieDaoImpl(sessionFactory);
+    public MovieServiceImpl(MovieDao dao) {
+        this.dao = dao;
     }
     
     @Override
