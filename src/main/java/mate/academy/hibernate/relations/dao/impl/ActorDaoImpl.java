016cd72aa49b77e3d484dc5945601dc6451c0bdd
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-27 15:20:31 +0300

    complete implementation of all classes in the `mate/academy/hibernate/relations/dao/impl` package

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,27 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.getTransaction();
+            session.save(actor);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't save actor to DB " + actor, e);
+        } finally {
+            transaction.rollback();
+        }
+        return actor;
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return Optional.ofNullable(super.factory.openSession().get(Actor.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get actor with id=" + id + " from DB", e);
+        }
     }
 }
