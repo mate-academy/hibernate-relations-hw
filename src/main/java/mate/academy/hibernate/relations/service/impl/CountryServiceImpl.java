3317a2ce83e2eb89c15d7784691f55e4b71c6bdb
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-28 15:47:20 +0200

    add improvements

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -5,7 +5,7 @@ import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
 public class CountryServiceImpl implements CountryService {
-    final private CountryDao countryDao;
+    private final CountryDao countryDao;
 
     public CountryServiceImpl(CountryDao countryDao) {
         this.countryDao = countryDao;
