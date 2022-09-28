9c5b8d68de801d53eb6afe7629e359b7ddfb1728
Vasyl Pankiv <wasilius2@gmail.com>
2022-09-28 13:45:07 +0300

    fix code style

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,13 +1,13 @@
 package mate.academy.hibernate.relations.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 public class Movie implements Cloneable {
