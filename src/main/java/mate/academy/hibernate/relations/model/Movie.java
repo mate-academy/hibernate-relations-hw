951264c1b4e41d07aef02931c3ea04317fa0afce
LubusUzh <lubus.uzh@gmail.com>
2022-09-28 12:25:54 +0300

    Add: entity, id and relations to Actor, Country, Movie classes

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,11 +1,20 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "movies")
 public class Movie implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "movies_actors",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "ctror_id"))
     private List<Actor> actors;
 
     public Movie() {
