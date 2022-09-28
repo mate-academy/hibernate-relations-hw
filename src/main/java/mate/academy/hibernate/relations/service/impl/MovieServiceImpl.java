cd0f868c504ef0ca645fa00b3dac881ad9181a0b
LubusUzh <lubus.uzh@gmail.com>
2022-09-28 14:05:29 +0300

    Add: implementation to service impl classes

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -4,13 +4,19 @@ import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private MovieService movieService;
+
+    public MovieServiceImpl(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return movieService.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return movieService.get(id);
     }
 }
