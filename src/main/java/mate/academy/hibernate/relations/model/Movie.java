906c9f51c07a6e47e03c7a8cd57f457ab770941e
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-09-28 13:46:15 +0300

    pom.xml was changed, hibernate.cfg.xml was created, @Entity @Id @GeneratedValue were added to models

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Movie implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private List<Actor> actors;
