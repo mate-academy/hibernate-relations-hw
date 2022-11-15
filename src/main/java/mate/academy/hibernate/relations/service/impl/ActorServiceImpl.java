48ae9f4a246125f9e02e5e5d526b8abafc398b96
shmax <mshveda28@gmail.com>
2022-11-15 14:31:17 +0200

    Implement service implementations

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,20 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.impl.GenericDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import mate.academy.hibernate.relations.service.exception.EntityNotFoundException;
 
-public class ActorServiceImpl implements ActorService {
-    @Override
-    public Actor add(Actor actor) {
-        return null;
+public class ActorServiceImpl extends GenericServiceImpl<Actor> implements ActorService {
+
+    public ActorServiceImpl(GenericDao<Actor> actorDao) {
+        super(actorDao);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        return dao.get(id).orElseThrow(()
+                -> new EntityNotFoundException("Actor not found with id "
+                + id));
     }
 }
