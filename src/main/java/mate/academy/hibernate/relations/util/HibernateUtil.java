abbf275d64aae060a13743c10008696d1e309902
shmax <mshveda28@gmail.com>
2022-11-15 14:05:58 +0200

    Configure hibernate; Implement model classes

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure()
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
     }
 }
