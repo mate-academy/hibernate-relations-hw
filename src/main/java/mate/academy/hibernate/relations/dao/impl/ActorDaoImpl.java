1629059730ca54c71e7f915242fa9bf12e083f91
Nazarii Pertsak <pertsak98@gmail.com>
2022-11-23 12:09:32 +0200

    implemented changes according to mentor review

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -37,9 +37,10 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Optional<Actor> get(Long id) {
-
         try (Session session = factory.openSession()) {
             return Optional.ofNullable(session.get(Actor.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get from db actor with id = " + id, e);
         }
     }
 }
