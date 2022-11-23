fc3bcdc1fb029af7e35ace2c69170933f130a26d
galenkoigor <galenkoigor@gmail.com>
2022-11-23 22:47:34 +0100

    Refactored Model classes after testing from Main

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -4,11 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "actors")
 public class Actor implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
