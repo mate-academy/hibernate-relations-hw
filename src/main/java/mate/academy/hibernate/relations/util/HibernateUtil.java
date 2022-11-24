c32c5a4d355cb60e90a089823a2771a59af51375
BP2117575 <marjanavv@gmail.com>
2022-11-24 12:03:57 +0100

    Hibernate-relation

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,19 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory instance = initSessionFactory();
+
+    public HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
+    }
+
+    public static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
     }
 }
