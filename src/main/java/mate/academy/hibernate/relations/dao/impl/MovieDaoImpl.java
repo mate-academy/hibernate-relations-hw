dc249401c4d90a8da79159cd8bd0c2c208cfd89b
Pavlo.Polovyi <pasha32662@gmail.com>
2022-12-01 16:59:54 +0200

    inlined variables in dao's.

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -39,8 +39,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
     @Override
     public Optional<Movie> get(Long id) {
         try (Session session = factory.openSession()) {
-            Movie movie = session.get(Movie.class, id);
-            return Optional.ofNullable(movie);
+            return Optional.ofNullable(session.get(Movie.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Can't get movie with id " + id, e);
         }
