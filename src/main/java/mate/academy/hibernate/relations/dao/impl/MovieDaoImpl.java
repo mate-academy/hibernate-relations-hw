1208e91be527962cda4f16d8b8db39eed86af80f
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-12-02 02:37:12 +0200

    services were instantiated using constructors, hibernate.cfg.xml was added

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -3,7 +3,6 @@ package mate.academy.hibernate.relations.dao.impl;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.exception.DataProcessingException;
-import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
