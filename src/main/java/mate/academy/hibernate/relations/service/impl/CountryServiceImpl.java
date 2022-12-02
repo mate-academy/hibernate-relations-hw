6ed14d0fb56c6763aa165f46d2b3a251ba8c1aa7
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-12-02 16:23:42 +0200

    fixed some issues in service level

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,14 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
-import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
     private CountryDao countryDao;
 
-    public CountryServiceImpl(SessionFactory sessionFactory) {
-        countryDao = new CountryDaoImpl(sessionFactory);
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
     }
 
     @Override
@@ -20,6 +18,7 @@ public class CountryServiceImpl implements CountryService {
 
     @Override
     public Country get(Long id) {
-        return countryDao.get(id).get();
+        return countryDao.get(id).orElseThrow(
+                () -> new RuntimeException("Couldn't get a country by id. " + id));
     }
 }
