c5d8fb53ad6d0af3cd08e6198189d2892381bc06
AlinaOryshak <alinaoryshak@gmail.com>
2022-12-03 14:20:53 +0200

    Changed imports

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,11 +1,10 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
 import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 import org.hibernate.SessionFactory;
-
-import java.util.NoSuchElementException;
 
 public class ActorServiceImpl implements ActorService {
     private final ActorDaoImpl actorDao;
