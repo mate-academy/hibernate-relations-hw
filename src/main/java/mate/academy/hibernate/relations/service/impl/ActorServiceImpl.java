21959423d843ef283cf4082ecf6ad8e6e5b8066e
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-12-04 13:17:02 +0200

    Make changes due to checkstyle

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,12 +1,11 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 import org.hibernate.SessionFactory;
-
-import java.util.NoSuchElementException;
 
 public class ActorServiceImpl implements ActorService {
     private final ActorDao actorDao;
