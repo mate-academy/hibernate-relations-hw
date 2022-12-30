a47375a0ddb4dadfcfafadb479b6f5c9106390d3
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-30 20:11:25 +0200

    mvn recommend

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -4,13 +4,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory instance = initSessionFactory();
+    private static final SessionFactory sessionFactory = initSessionFactory();
 
     private static SessionFactory initSessionFactory() {
         return new Configuration().configure().buildSessionFactory();
     }
 
     public static SessionFactory getSessionFactory() {
-        return instance;
+        return sessionFactory;
     }
 }
