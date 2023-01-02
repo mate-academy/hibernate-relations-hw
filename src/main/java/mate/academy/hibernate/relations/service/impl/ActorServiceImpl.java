30d35eeec3016db5246d564930d1a2ebc4722076
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-02 14:55:49 +0200

    add throw exceptions in service layer

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -2,6 +2,7 @@ package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 import org.hibernate.SessionFactory;
@@ -20,6 +21,8 @@ public class ActorServiceImpl implements ActorService {
 
     @Override
     public Actor get(Long id) {
-        return actorDao.get(id).orElse(null);
+        return actorDao.get(id).orElseThrow(
+                () -> new DataProcessingException(
+                        "Actor by Id: " + id + " doesn't excist or database' error"));
     }
 }
