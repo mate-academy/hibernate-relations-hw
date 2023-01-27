35790969cfd2a169f39861348bed76eb3ca25c51
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-01-27 13:20:26 +0200

    JoinColumns(nullable = false) was removed from Actor model

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -16,7 +15,6 @@ public class Actor implements Cloneable {
     private Long id;
     private String name;
     @ManyToOne
-    @JoinColumn(nullable = false)
     private Country country;
 
     public Actor() {
