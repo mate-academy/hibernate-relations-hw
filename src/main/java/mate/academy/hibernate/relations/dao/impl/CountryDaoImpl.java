9b950a9fd3a03a58c4611ea5828c574ca42e2563
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-02-08 12:13:19 +0000

    Update src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
    Co-authored-by: Leonid Sivko <rommel@ukr.net>

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -22,7 +22,7 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
             transaction = session.beginTransaction();
             session.save(country);
             transaction.commit();
-        } catch (DataProcessingException e) {
+        } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
