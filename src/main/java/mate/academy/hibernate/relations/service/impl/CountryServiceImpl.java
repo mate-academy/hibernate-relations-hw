fbe3fdeba6712981511cc8ba67266527eccf386e
Sergii Bezrukov <73116558+finedefinition@users.noreply.github.com>
2023-02-08 12:53:19 +0000

    Clean code v 1.0

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,10 +1,8 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
-import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
     private CountryDao countryDao;
