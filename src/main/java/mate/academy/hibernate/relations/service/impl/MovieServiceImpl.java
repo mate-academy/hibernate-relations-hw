30b5ab09b941f0c5103b4902cc167f1f0d8ea8b4
Denis Pugovkin <den.pugovkin@gmail.com>
2023-02-25 14:54:57 +0200

    1. Hibernate was added and configured. 2. Implementation of all models was completed. 3. Added own exception and throw it in DAO layer.

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,23 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
+import mate.academy.hibernate.relations.util.HibernateUtil;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao = new MovieDaoImpl(HibernateUtil.getSessionFactory());
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return movieDao.get(id)
+                .orElseThrow(() -> new NoSuchElementException("Couldn't find movie by id " + id));
     }
 }
