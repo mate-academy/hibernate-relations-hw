e09142972545ca7b9734fed3c1aaba585b9898cc
Nazarii Kharyshyn <nazariykharyshyn@gmail.com>
2023-03-12 14:09:44 +0200

    Fixes after review -added catch blocks for all operations in DAO layer

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -37,6 +37,8 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public Optional<Country> get(Long id) {
         try (Session session = factory.openSession()) {
             return Optional.ofNullable(session.get(Country.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get a country by id: " + id, e);
         }
     }
 }
