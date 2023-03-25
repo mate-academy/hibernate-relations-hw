3b2cd91a194a732b53532cf86f8207fcaa609a50
codered23 <oleg2003andriy@gmail.com>
2023-03-26 01:36:35 +0200

    task solved

--- src/main/java/mate/academy/hibernate/relations/model/Country.java
+++ src/main/java/mate/academy/hibernate/relations/model/Country.java
@@ -1,6 +1,16 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name = "country")
 public class Country implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
