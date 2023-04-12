fd1101a346f7ad6b9d3a6bd2e2c2efe3f94d45a6
Viacheslav Kyslyi <kyslyiviacheslav@gmail.com>
2023-04-12 20:32:24 +0300

    added space 5) completed implementation of all classes in the mate/academy/hibernate/relations/service/impl/ package 6) created hibernate.cfg.xml file and implemented mate/academy/hibernate/relations/util/HibernateUtil class 7) made mate/academy/Main.java work;

--- src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -2,6 +2,6 @@ package mate.academy.hibernate.relations.exception;
 
 public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Exception e) {
-        super(message,e);
+        super(message, e);
     }
 }
