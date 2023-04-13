449f464dd15a6a1f89f22e28b93692d843643ec6
Sushko Roman <formatesus@gmail.com>
2023-04-13 12:46:40 +0300

    added constructor to HibernateUtil

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -6,6 +6,9 @@ import org.hibernate.cfg.Configuration;
 public class HibernateUtil {
     private static final SessionFactory instance = initSessionFactory();
 
+    public HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
         return instance;
     }
