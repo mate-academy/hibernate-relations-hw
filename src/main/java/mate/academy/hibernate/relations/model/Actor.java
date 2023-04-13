1abb343e80d3418e8cdf4f2ea8b81bf5764ea151
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-04-13 10:19:25 +0300

    added relations

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,11 +1,29 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "actor")
 public class Actor implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Country country;
 
     public Actor() {
+    }
+
+    public Actor(String name, Country country) {
+        this.name = name;
+        this.country = country;
     }
 
     public Actor(String name) {
