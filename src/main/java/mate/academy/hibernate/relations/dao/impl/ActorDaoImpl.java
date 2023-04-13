21e88ff18db8b027a3a3e975bbd793c7e4508b7b
Maksym <maxim.yashyn@gmail.com>
2023-04-13 18:41:07 +0300

    fixes after mentor review

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -26,7 +26,7 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't add actor to DB: " + actor);
+            throw new DataProcessingException("Can't add actor to DB: " + actor, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -40,7 +40,7 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
         try (Session session = factory.openSession()) {
             return Optional.ofNullable(session.get(Actor.class, id));
         } catch (Exception e) {
-            throw new DataProcessingException("Can't get actor from DB by ID: " + id);
+            throw new DataProcessingException("Can't get actor from DB by ID: " + id, e);
         }
     }
 }
