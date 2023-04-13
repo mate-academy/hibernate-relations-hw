8bdac1713c006087b57485485949f5644d90b747
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-04-13 15:30:25 +0300

    resolved issues

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -21,6 +21,10 @@ public class Actor implements Cloneable {
     public Actor() {
     }
 
+    public Actor(String name) {
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
