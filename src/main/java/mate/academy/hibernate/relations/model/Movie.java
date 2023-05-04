046b38481c264b2314f8a94675d0ce60a74219e8
Artur Muliukov <arthur.muliukov@gmail.com>
2023-05-04 13:43:30 +0200

    Solved 1.4

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,7 +1,6 @@
 package mate.academy.hibernate.relations.model;
 
 import jakarta.persistence.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +12,9 @@ public class Movie implements Cloneable {
     private Long id;
     private String title;
     @ManyToMany
+    @JoinTable(name = "movies_actors",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id"))
     private List<Actor> actors;
 
     public Movie() {
