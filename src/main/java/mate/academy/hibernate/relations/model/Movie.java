4ba7c5047de85bb0ac1b164980344ecb0da95bba
Valerii <kulibchenkovalera@gmail.com>
2023-05-25 14:29:13 +0300

    First commit

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -2,10 +2,27 @@ package mate.academy.hibernate.relations.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "movies")
 public class Movie implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "movies_actors",
+            joinColumns = @JoinColumn(name = "movies_id"),
+            inverseJoinColumns = @JoinColumn (name = "actors_id"))
     private List<Actor> actors;
 
     public Movie() {
@@ -60,7 +77,8 @@ public class Movie implements Cloneable {
     public String toString() {
         return "Movie{"
                 + "id=" + id
-                + ", title='" + title + '\''
+                + ", title='" + title
+                + ", actors=" + actors
                 + '}';
     }
 }
