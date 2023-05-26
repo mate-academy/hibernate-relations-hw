b7a35fc7e28f4dab1f145961ddf0e1f493dbeab6
Vlad Polikarpov <vladpolikarpovvp@gmail.com>
2023-05-26 21:15:47 +0300

    added proper solution for this task

--- src/main/java/mate/academy/hibernate/relations/model/Country.java
+++ src/main/java/mate/academy/hibernate/relations/model/Country.java
@@ -1,6 +1,12 @@
 package mate.academy.hibernate.relations.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "countries")
 public class Country implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
@@ -38,9 +44,6 @@ public class Country implements Cloneable {
 
     @Override
     public String toString() {
-        return "Country{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + '}';
+        return "Country{" + "id=" + id + ", name='" + name + '\'' + '}';
     }
 }
