44266349b02b197d09bcc025ad2f559a56bd5116
Vladyslav Krutyus <kijdron@gmail.com>
2023-06-20 14:45:12 +0300

    Services now have SessionFactory instead of individual DAOs as a parameter

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -2,14 +2,17 @@ package mate.academy.hibernate.relations.service.impl;
 
 import java.util.NoSuchElementException;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
     private ActorDao actorDao;
 
-    public ActorServiceImpl(ActorDao actorDao) {
-        this.actorDao = actorDao;
+    public ActorServiceImpl(SessionFactory sessionFactory) {
+        this.actorDao = new ActorDaoImpl(sessionFactory);
     }
 
     @Override
