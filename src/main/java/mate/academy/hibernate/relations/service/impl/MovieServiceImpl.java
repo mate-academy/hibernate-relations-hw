48e743af52ef70f3cb16367e16432ddeb3579302
Oleksii Parkhomenko <kcas.pilot@gmail.com>
2023-06-22 20:51:56 +0300

    hibernate-relations-hw

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,25 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
+import mate.academy.hibernate.relations.lib.Inject;
+import mate.academy.hibernate.relations.lib.Service;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
+@Service
 public class MovieServiceImpl implements MovieService {
+    @Inject
+    private MovieDao movieDao;
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return movieDao.get(id).orElseThrow(() ->
+                new DataProcessingException("Can't get movie with ID: " + id));
     }
 }
