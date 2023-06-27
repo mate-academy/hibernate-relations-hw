94d665b63ea621f8c752488832ec520de670439a
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-06-27 11:06:22 +0300

    finally version after student revue (removed unnecessary Fetch Type).

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,7 +1,6 @@
 package mate.academy.hibernate.relations.model;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,7 +14,7 @@ public class Actor implements Cloneable {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @ManyToOne (fetch = FetchType.EAGER)
+    @ManyToOne
     private Country country;
 
     public Actor() {
