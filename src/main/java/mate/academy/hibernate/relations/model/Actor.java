720ef47896b7f017d0380ef227234f6b8e6f9c82
Andrii <subashandrii@gmail.com>
2023-07-03 16:31:39 +0300

    home-work

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,6 +1,11 @@
 package mate.academy.hibernate.relations.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "actors")
@@ -9,7 +14,7 @@ public class Actor implements Cloneable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @OneToOne
+    @ManyToOne
     private Country country;
 
     public Actor() {
