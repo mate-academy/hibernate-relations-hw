a1d66e157bb3f89bebf40ca5d723cca4aa11c88b
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-07-05 00:05:19 +0300

    Fix exception after MR

--- src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -5,4 +5,8 @@ public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message) {
         super(message);
     }
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
