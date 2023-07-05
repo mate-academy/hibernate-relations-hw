76344e3ef7d6c26dbef7adaa54cec3f1a9e7bafc
Igor Maksimov <maksigors@gmail.com>
2023-07-05 16:13:05 +0300

    build 1.2 improved

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -1,12 +1,6 @@
 package mate.academy.hibernate.relations;
 
 import java.util.List;
-import mate.academy.hibernate.relations.dao.ActorDao;
-import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.dao.MovieDao;
-import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
-import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
-import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.model.Movie;
@@ -23,13 +17,11 @@ public class Main {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-        CountryDao countryDao = new CountryDaoImpl(sessionFactory);
-        CountryService countryService = new CountryServiceImpl(countryDao);
+        CountryService countryService = new CountryServiceImpl(sessionFactory);
         Country usa = new Country("USA");
         countryService.add(usa);
 
-        ActorDao actorDao = new ActorDaoImpl(sessionFactory);
-        ActorService actorService = new ActorServiceImpl(actorDao);
+        ActorService actorService = new ActorServiceImpl(sessionFactory);
         Actor vinDiesel = new Actor("Vin Diesel");
         vinDiesel.setCountry(usa);
         actorService.add(vinDiesel);
@@ -38,8 +30,7 @@ public class Main {
         momoa.setCountry(usa);
         actorService.add(momoa);
 
-        MovieDao movieDao = new MovieDaoImpl(sessionFactory);
-        MovieService movieService = new MovieServiceImpl(movieDao);
+        MovieService movieService = new MovieServiceImpl(sessionFactory);
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setActors(List.of(vinDiesel, momoa));
         movieService.add(fastAndFurious);
