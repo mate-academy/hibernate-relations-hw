157d7cae1a366318c95ac7f48efaf239391126d1
Petro Kobevko <kobevkopi@gmail.com>
2023-07-10 10:17:40 +0200

    Refactored code according to the student's comments

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -19,6 +20,7 @@ public class Actor implements Cloneable {
     private String name;
 
     @ManyToOne
+    @JoinColumn(name = "country_id")
     private Country country;
 
     public Actor() {
