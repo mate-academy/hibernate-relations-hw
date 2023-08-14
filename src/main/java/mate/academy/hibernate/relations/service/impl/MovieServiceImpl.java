f148cf06bd7681dd4b1c7a4d3fc7be275dafd025
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-08-15 01:08:04 +0300

    replaced classes with interfaces in services' implementations

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,12 +1,13 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 import org.hibernate.SessionFactory;
 
 public class MovieServiceImpl implements MovieService {
-    private final MovieDaoImpl movieDao;
+    private final MovieDao movieDao;
 
     public MovieServiceImpl(SessionFactory sessionFactory) {
         this.movieDao = new MovieDaoImpl(sessionFactory);
