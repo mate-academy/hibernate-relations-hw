4ced365e19de5f2f6f248eb1832b29c1c3320358
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-08-15 12:24:35 +0300

    added empty line at end of hibernate.cfg.xml, replaced RuntimeException by NoSuchElementException in services, added @JoinColumn in Actor

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
@@ -13,6 +14,7 @@ public class Actor implements Cloneable {
     private Long id;
     private String name;
     @ManyToOne
+    @JoinColumn(name = "country_id")
     private Country country;
 
     public Actor() {
