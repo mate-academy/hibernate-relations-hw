f086418a181a5c3c46714e8f725e77ff87711af0
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-08-15 15:49:26 +0300

    Ñrefactored code according suggestions

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,11 +1,6 @@
 package mate.academy.hibernate.relations.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "actors")
@@ -15,6 +10,7 @@ public class Actor implements Cloneable {
     private Long id;
     private String name;
     @ManyToOne
+    @JoinColumn(name = "country_id")
     private Country country;
 
     public Actor() {
