bca9ecf59e9b2b4e15ce6183b3532935e87d7512
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-08-15 13:18:28 +0300

    added solution for hibernate_relations

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,34 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(actor);
+            if (session == null) {
+                throw new RuntimeException("There is no value to save!");
+            }
+            transaction.commit();
+        } catch (RuntimeException e) {
+            transaction.rollback();
+            throw new DataProcessingException("Error occurred during transaction", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return actor;
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return Optional.ofNullable(session.get(Actor.class, id));
+        } catch (Throwable e) {
+            throw new DataProcessingException("Can't get actor"
+                    + "by provided id " + id, e);
+        }
     }
 }
