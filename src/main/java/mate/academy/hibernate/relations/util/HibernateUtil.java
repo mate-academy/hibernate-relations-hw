90ac4194222b33ab9970f30b38c76a5f39057967
Visqe <veetee@tutanota.com>
2023-08-16 09:23:00 +0300

    improvements

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -3,6 +3,9 @@ package mate.academy.hibernate.relations.util;
 import org.hibernate.SessionFactory;
 
 public class HibernateUtil {
+    private HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
         return null;
     }
