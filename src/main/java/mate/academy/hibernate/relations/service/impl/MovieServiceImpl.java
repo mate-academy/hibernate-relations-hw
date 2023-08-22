64b2fb01ecb2e67b4c48d5e459aaa2e0ab8d9c17
Sergey Shestaka <sergey.shestaka@gmail.com>
2023-08-22 23:29:24 +0200

    1. Required hibernate dependencies included. 2. Implementation of all Models finished. 3. Implementation of all Dao finished. 4. Implementation of all Services finished. 5. Custom unchecked DataProcessingException created. 6. File `hibernate.cfg.xml` created. 7. HibernateUtil class implemented. 8. All tasks in Main are working. 9. All tests passed.

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,32 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
+import org.hibernate.SessionFactory;
 
-public class MovieServiceImpl implements MovieService {
+public class MovieServiceImpl extends AbstractService implements MovieService {
+    private static MovieDao movieDao;
+
+    public MovieServiceImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+        initMovieDao();
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return movieDao.get(id).orElseThrow(() ->
+                new NoSuchElementException("Can't get movie by id: " + id));
+    }
+
+    private void initMovieDao() {
+        movieDao = new MovieDaoImpl(factory);
     }
 }
