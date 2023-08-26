886a0afe1dc14ef49710f4c56a7dbb41ae640cf6
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-08-27 02:54:16 +0300

    added fetch type

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -3,6 +3,7 @@ package mate.academy.hibernate.relations.model;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -19,7 +20,7 @@ public class Movie implements Cloneable {
     private Long id;
     private String title;
 
-    @OneToMany()
+    @OneToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "movies_actors",
             joinColumns = @JoinColumn(name = "movie_id"),
