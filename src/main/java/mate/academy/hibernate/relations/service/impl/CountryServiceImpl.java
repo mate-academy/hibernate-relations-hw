e311de5a7d694e422b930f470635755586ca73a6
Yuriy Kononchuk <y_kononchuk@ukr.net>
2023-08-30 20:59:50 +0300

    added solution for Hibernate_Relations

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,28 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
+import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
+    private final SessionFactory factory;
+
+    public CountryServiceImpl(SessionFactory factory) {
+        this.factory = factory;
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        CountryDao countryDao = new CountryDaoImpl(factory);
+        return countryDao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        CountryDao countryDao = new CountryDaoImpl(factory);
+        return countryDao.get(id).orElseThrow(() -> new RuntimeException("ID country " + id
+                + " weren't found"));
     }
 }
