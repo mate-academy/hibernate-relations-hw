a530bbcd44da2f951b4457c343548268efca452e
Sergei <chertterner686@gmail.com>
2023-09-12 19:51:53 +0300

    fixed after mentors check

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -2,10 +2,8 @@ package mate.academy.hibernate.relations.service.impl;
 
 import java.util.NoSuchElementException;
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
-import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
     private CountryDao countryDao;
