1a0805beb23544ca8fb5b991a466b9547066e8da
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-09-23 21:51:54 +0300

    first version, all tests ok

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,6 +1,12 @@
 package mate.academy.hibernate.relations.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "actors")
