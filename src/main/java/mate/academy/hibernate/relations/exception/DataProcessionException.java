fd94bacfef6c6bbf92e311a3371ac899518c052f
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-26 19:04:01 +0300

    implemented task by usage of AbstractDao class

---
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessionException.java
@@ -0,0 +1,7 @@
+package mate.academy.hibernate.relations.exception;
+
+public class DataProcessionException extends RuntimeException {
+    public DataProcessionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,27 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
+import org.hibernate.SessionFactory;
 
 public class MovieServiceImpl implements MovieService {
+    private final SessionFactory sessionFactory;
+
+    public MovieServiceImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        MovieDao movieDao = new MovieDaoImpl(sessionFactory);
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        MovieDao movieDao = new MovieDaoImpl(sessionFactory);
+        return movieDao.get(id).get();
     }
 }
