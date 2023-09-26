80705b1fb75269f4debfae729c6407057a6cd8e8
Oleksandr Yablunovskyi <briva.study@gmail.com>
2023-09-26 15:07:20 +0300

    Hibernate-relations task is implemented

--- src/main/java/mate/academy/hibernate/relations/dao/impl/AbstractDao.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/AbstractDao.java
@@ -1,11 +1,36 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import mate.academy.hibernate.relations.exception.DataProcessingException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public abstract class AbstractDao {
     protected final SessionFactory factory;
 
     protected AbstractDao(SessionFactory sessionFactory) {
         this.factory = sessionFactory;
+    }
+    
+    protected <T> T add(T entity) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException(
+                    "Can't add %s to DB".formatted(entity.getClass().getSimpleName()), e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return entity;
     }
 }
