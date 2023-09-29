8d8e6dc933b5a635adf5c115bd27047e06cf5af5
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-09-29 10:52:40 +0300

    mvn fixed

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -20,6 +20,6 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public Movie get(Long id) {
         return movieDao.get(id).orElseThrow(() -> new DataProcessionException(
-                "Cannot find movie by id"+ id));
+                "Cannot find movie by id" + id));
     }
 }
