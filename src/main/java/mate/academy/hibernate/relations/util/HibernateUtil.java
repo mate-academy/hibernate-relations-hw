5734e798bf93a41f01a4a9d6b1d1aa389e3bcf22
Max Skory <devisrush@gmail.com>
2023-10-01 14:42:14 +0300

    task was implemented

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,22 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static volatile SessionFactory factory;
+
+    private HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        if (factory == null) {
+            synchronized (HibernateUtil.class) {
+                if (factory == null) {
+                    factory = new Configuration().configure().buildSessionFactory();
+                }
+            }
+        }
+        return factory;
     }
 }
