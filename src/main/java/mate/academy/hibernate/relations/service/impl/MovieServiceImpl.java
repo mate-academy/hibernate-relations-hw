a0f1ebf344c055b39933c3dcfcbcbe806d60fa03
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-10-01 11:24:30 -0500

    Changed constructor arguments in ActorServiceImpl, CountryServiceImpl and CountryServiceImpl classes

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,14 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.MovieDao;
-import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
-import org.hibernate.SessionFactory;
 
 public class MovieServiceImpl implements MovieService {
     private MovieDao dao;
 
-    public MovieServiceImpl(SessionFactory sessionFactory) {
-        dao = new MovieDaoImpl(sessionFactory);
+    public MovieServiceImpl(MovieDao dao) {
+        this.dao = dao;
     }
 
     @Override
