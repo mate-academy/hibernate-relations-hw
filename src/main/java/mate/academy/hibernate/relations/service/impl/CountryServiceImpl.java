3413460f459c106e7af5b3930806e22eac6951de
Anatolii Nazarov <anatoly_n@ukr.net>
2023-10-04 16:57:46 +0300

    Fixed after remarks

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,10 +1,9 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import javax.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
-
-import javax.persistence.EntityNotFoundException;
 
 public class CountryServiceImpl implements CountryService {
     private final CountryDao countryDao;
