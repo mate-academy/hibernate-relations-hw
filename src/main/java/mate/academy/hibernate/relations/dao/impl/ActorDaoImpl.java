4f0fea9264fe80db04f739e6b8516b3755f0e1b1
Danylo <kdanil2000@gmail.com>
2023-10-10 15:26:07 +0300

    Hibernate reletions impl

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -1,9 +1,13 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import java.util.Objects;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,30 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
+            session.persist(actor);
+            transaction.commit();
+        } catch (Exception e) {
+            Objects.requireNonNull(transaction).rollback();
+            throw new DataProcessingException("Can't add actor to dataBase", e);
+        } finally {
+            Objects.requireNonNull(session).close();
+        }
+        return actor;
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            Actor actorFromDb = session.get(Actor.class, id);
+            return Optional.ofNullable(actorFromDb);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get actor from dataBase by whom id: " + id, e);
+        }
     }
 }
