5b867fb574b887a8ab4832197615da6c16264554
Yurii Trokhymenko <uta1965_@ukr.net>
2023-10-28 14:53:23 +0200

    edits methods ActorDaoImpl, ActorServiceImpl, CountryDaoImpl, CountryServiceImpl, HibernateUtil, MovieDaoImpl, MovieServiceImpl, create file hibernate.cfg.xml, method DataProcessingException, create annotations in classes Actor, Country, Movie, edit clesses Main, edit file pom.xml

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,28 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
+import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
+    private SessionFactory factory;
+    private CountryDao countryDao = new CountryDaoImpl(factory);
+
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        return countryDao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        return countryDao.get(id).orElseThrow(() ->
+                new RuntimeException("Can't get country "
+                    + "with id " + id));
     }
 }
