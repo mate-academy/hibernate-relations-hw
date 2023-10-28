a56af6ce44b26272ade2dce7a99c5223f1843e3d
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-10-28 19:16:19 +0300

    added the implementation

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -40,7 +40,7 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
     @Override
     public Optional<Country> get(Long id) {
         try (Session session = factory.openSession()) {
-            return Optional.of(session.get(Country.class, id));
+            return Optional.ofNullable(session.get(Country.class, id));
         }
     }
 }
