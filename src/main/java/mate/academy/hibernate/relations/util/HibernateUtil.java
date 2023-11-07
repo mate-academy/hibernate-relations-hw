ad35f6050970246e7e9e972a1c4e893a26b27225
Solomia Bobyak <mia.bobyak@gmail.com>
2023-11-07 19:44:25 +0200

    Added HibernateUtil class to manage Hibernate sessions

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,20 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory instance = initSessionFactory();
+
+    private HibernateUtil() {
+        throw new AssertionError("HibernateUtil should not be instantiated.");
+    }
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
     }
 }
