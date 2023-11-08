bc2a9a5509bb6cee8d32011ba67e9db3ea6607df
Solomia Bobyak <mia.bobyak@gmail.com>
2023-11-08 18:08:36 +0200

    Added exception

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -2,6 +2,7 @@ package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
+import mate.academy.hibernate.relations.exeption.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 import org.hibernate.SessionFactory;
@@ -20,6 +21,7 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Movie get(Long id) {
-        return movieDao.get(id).orElse(null);
+        return movieDao.get(id)
+                .orElseThrow(() -> new DataProcessingException("Failed to get movie with id: " + id, new Exception()));
     }
 }
