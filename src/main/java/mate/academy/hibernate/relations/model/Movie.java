d901a15409c1dccf0a0bf1b19b6998944ec0bde4
Solomia Bobyak <mia.bobyak@gmail.com>
2023-11-08 20:43:33 +0200

    changed the annotation for the connection between entities

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -6,7 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +19,12 @@ public class Movie implements Cloneable {
     private Long id;
     private String title;
 
-    @OneToMany
+    @ManyToMany
+    @JoinTable(
+            name = "movie_actor",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
     private List<Actor> actors;
 
     public Movie() {
