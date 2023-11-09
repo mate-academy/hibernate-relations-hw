5efd515947b006d107f1462efc7d1de464d8afde
Vitalii <stelmaschyk.vg@gmail.com>
2023-11-09 15:27:29 +0200

    Solution for task hibernate_relations, first commit

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,29 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.impl.AbstractDao;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.hibernate.SessionFactory;
 
-public class ActorServiceImpl implements ActorService {
+public class ActorServiceImpl extends AbstractDao implements ActorService {
+    private ActorDao actorDao = new ActorDaoImpl(factory);
+
+    public ActorServiceImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        actorDao.add(actor);
+        return actor;
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        Optional<Actor> actorOptional = actorDao.get(id);
+        return actorOptional.orElse(null);
     }
 }
