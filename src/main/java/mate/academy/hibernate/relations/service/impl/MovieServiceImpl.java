c54055197d9bf1dda3a8bccfc255b866ee8762fb
Anton Babych <antonbabi13@gmail.com>
2023-11-12 18:01:31 +0200

    Fix problem, change actor - country from OneToOne to ManyToOne, also fix single responsibility principal and now service don`t create dao object;

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -2,17 +2,15 @@ package mate.academy.hibernate.relations.service.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.MovieDao;
-import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
-import org.hibernate.SessionFactory;
 
 public class MovieServiceImpl implements MovieService {
 
     private final MovieDao movieDao;
 
-    public MovieServiceImpl(SessionFactory sessionFactory) {
-        this.movieDao = new MovieDaoImpl(sessionFactory);
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
     }
 
     @Override
