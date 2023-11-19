170a2e4055e92df8fc84aead619d56ad9195b1a5
Nikita <kaptetos@gmail.com>
2023-11-19 10:56:48 +0200

    the class serves as a layer between the data access layer and the application layer

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,39 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.List;
+import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
 public class ActorServiceImpl implements ActorService {
+    private final ActorDao actorDao;
+
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        return actorDao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        return actorDao.get(id).orElse(null);
+    }
+
+    @Override
+    public List<Actor> getAll() {
+        return actorDao.getAll();
+    }
+
+    @Override
+    public Actor update(Actor actor) {
+        return actorDao.update(actor);
+    }
+
+    @Override
+    public void delete(Long id) {
+        actorDao.delete(id);
     }
 }
