b8728f0732c91fdb5aa88fba047dc1131a0aa75f
Nikita <kaptetos@gmail.com>
2023-11-19 11:01:23 +0200

    CountryDaoImpl is a class that implements the CountryDao interface and is responsible for interacting with the database to perform CRUD operations

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import java.util.List;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
@@ -18,5 +19,20 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
     @Override
     public Optional<Country> get(Long id) {
         return null;
+    }
+
+    @Override
+    public List<Country> getAll() {
+        return null;
+    }
+
+    @Override
+    public Country update(Country country) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
     }
 }
