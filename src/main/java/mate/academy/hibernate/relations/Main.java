4a4fdf0ba6373cfb1cb4a52d3c3da80723163309
Nikita <kaptetos@gmail.com>
2023-11-19 10:38:18 +0200

    Main method with interaction with services and database operations

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -1,33 +1,46 @@
 package mate.academy.hibernate.relations;
 
 import java.util.List;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
+import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.ActorService;
 import mate.academy.hibernate.relations.service.CountryService;
 import mate.academy.hibernate.relations.service.MovieService;
+import mate.academy.hibernate.relations.service.impl.ActorServiceImpl;
+import mate.academy.hibernate.relations.service.impl.CountryServiceImpl;
+import mate.academy.hibernate.relations.service.impl.MovieServiceImpl;
 import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        // use this session factory when you will initialize service instances
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-        Country usa = new Country("USA");
-        CountryService countryService = null; // TODO: initialize this instance
-        countryService.add(usa);
+        CountryService countryService = new CountryServiceImpl(new CountryDaoImpl(sessionFactory));
+        ActorService actorService = new ActorServiceImpl(new ActorDaoImpl(sessionFactory));
+        MovieService movieService = new MovieServiceImpl(new MovieDaoImpl(sessionFactory));
 
-        Actor vinDiesel = new Actor("Vin Diesel");
-        vinDiesel.setCountry(usa);
-        ActorService actorService = null; // TODO: initialize this instance
-        actorService.add(vinDiesel);
+        try {
+            Country usa = new Country("USA");
+            countryService.add(usa);
 
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setActors(List.of(vinDiesel));
-        MovieService movieService = null; // TODO: initialize this instance
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
+            Actor vinDiesel = new Actor("Vin Diesel");
+            vinDiesel.setCountry(usa);
+            actorService.add(vinDiesel);
+
+            Movie fastAndFurious = new Movie("Fast and Furious");
+            fastAndFurious.setActors(List.of(vinDiesel));
+            movieService.add(fastAndFurious);
+
+            System.out.println(movieService.get(fastAndFurious.getId()));
+        } catch (DataProcessingException e) {
+            System.err.println("Error while processing data: " + e.getMessage());
+        } finally {
+            HibernateUtil.shutdown();
+        }
     }
 }
