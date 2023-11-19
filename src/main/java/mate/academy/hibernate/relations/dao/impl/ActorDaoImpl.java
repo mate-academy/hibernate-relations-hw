a7e25b41d44c708b0e28c34df51a1484c0afeb7f
Nikita <kaptetos@gmail.com>
2023-11-19 10:56:03 +0200

    the class provides implementation of CRUD operations for the Actor entity in the database

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import java.util.List;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
@@ -18,5 +19,20 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
     @Override
     public Optional<Actor> get(Long id) {
         return null;
+    }
+
+    @Override
+    public List<Actor> getAll() {
+        return null;
+    }
+
+    @Override
+    public Actor update(Actor actor) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
     }
 }
