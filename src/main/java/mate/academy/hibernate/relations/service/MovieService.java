1cbcba2a9ccc10c598fb77b743ca00b07a198125
Nikita <kaptetos@gmail.com>
2023-11-19 10:33:09 +0200

    added code in the interface

--- src/main/java/mate/academy/hibernate/relations/service/MovieService.java
+++ src/main/java/mate/academy/hibernate/relations/service/MovieService.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.service;
 
+import java.util.List;
 import mate.academy.hibernate.relations.model.Movie;
 
 public interface MovieService {
     Movie add(Movie movie);
 
     Movie get(Long id);
+
+    List<Movie> getAll();
+
+    Movie update(Movie movie);
+
+    void delete(Long id);
 }
