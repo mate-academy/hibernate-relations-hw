5d3f70094620a629c4a9d9bc4b3968d95916f419
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2023-11-20 13:53:51 +0200

    Add fixes with mentor's advices: add actors annotations in Movie delete fetch type above country in Actor

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -6,7 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +19,12 @@ public class Movie implements Cloneable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    @OneToMany (fetch = FetchType.LAZY)
-    @JoinColumn(name = "movies_id")
+    @ManyToMany (fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "movies_actors",
+            joinColumns = @JoinColumn(name = "actors_id"),
+            inverseJoinColumns = @JoinColumn(name = "movies_id")
+    )
     private List<Actor> actors;
 
     public Movie() {
