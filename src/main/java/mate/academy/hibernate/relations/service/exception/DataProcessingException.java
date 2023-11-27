80470756b12642c5d794c9f249b0e7a30640d429
Roman Globa <globaroman@gmail.com>
2023-11-27 22:29:56 +0200

    Implemened steps in accordance task

---
+++ src/main/java/mate/academy/hibernate/relations/service/exception/DataProcessingException.java
@@ -0,0 +1,11 @@
+package mate.academy.hibernate.relations.service.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
