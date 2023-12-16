8444024a0e2c440c313c64e69f452e74636adc3f
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-16 22:45:37 +0200

    added changes to the class Main

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -7,9 +7,6 @@ import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.ActorService;
 import mate.academy.hibernate.relations.service.CountryService;
 import mate.academy.hibernate.relations.service.MovieService;
-import mate.academy.hibernate.relations.service.impl.ActorServiceImpl;
-import mate.academy.hibernate.relations.service.impl.CountryServiceImpl;
-import mate.academy.hibernate.relations.service.impl.MovieServiceImpl;
 import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
@@ -17,21 +14,20 @@ public class Main {
     public static void main(String[] args) {
         // use this session factory when you will initialize service instances
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        CountryService countryService = new CountryServiceImpl();
-        // TODO: Initialize with the actual implementation
-        ActorService actorService = new ActorServiceImpl();
-        // TODO: Initialize with the actual implementation
-        MovieService movieService = new MovieServiceImpl();
-        // TODO: Initialize with the actual implementation
+
         Country usa = new Country("USA");
+        CountryService countryService = null; // TODO: initialize this instance
         countryService.add(usa);
+
         Actor vinDiesel = new Actor("Vin Diesel");
         vinDiesel.setCountry(usa);
+        ActorService actorService = null; // TODO: initialize this instance
         actorService.add(vinDiesel);
+
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setActors(List.of(vinDiesel));
+        MovieService movieService = null; // TODO: initialize this instance
         movieService.add(fastAndFurious);
-        Movie retrievedMovie = movieService.get(fastAndFurious.getId());
-        System.out.println("Retrieved Movie: " + retrievedMovie);
+        System.out.println(movieService.get(fastAndFurious.getId()));
     }
 }
