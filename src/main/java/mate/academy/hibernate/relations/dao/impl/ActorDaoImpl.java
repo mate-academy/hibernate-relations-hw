1298dae250921dd1b58c493390f16db8c4bd0bfa
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-16 22:35:03 +0200

    added changes to the class ActorDaoImpl

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -38,7 +38,7 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Optional<Actor> get(Long id) {
-        try (Session session =HibernateUtil.getSessionFactory().openSession()) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return Optional.ofNullable(session.get(Actor.class, id));
         }
     }
