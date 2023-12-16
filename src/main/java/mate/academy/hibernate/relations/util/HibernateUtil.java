3fb77d114f1416b55edb27d405a8d50f7f54762a
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-16 22:34:27 +0200

    added changes to the class HibernateUtil

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -6,6 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
     private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
     public static SessionFactory buildSessionFactory() {
         try {
             return new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
@@ -14,6 +15,7 @@ public class HibernateUtil {
             throw new ExceptionInInitializerError(e);
         }
     }
+
     public static SessionFactory getSessionFactory() {
         return SESSION_FACTORY;
     }
