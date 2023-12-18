044259f04ad0fec516fe91e92baac29ebcd47b79
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-18 16:05:18 +0200

    added changes to the class CountryServiceImpl

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,13 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.dao.DataProcessingException;
-import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
-import mate.academy.hibernate.relations.util.HibernateUtil;
-
-import java.util.Optional;
 
 public class CountryServiceImpl implements CountryService {
     private final CountryDao countryDao;
+
     public CountryServiceImpl(CountryDao countryDao) {
         this.countryDao = countryDao;
     }
@@ -19,6 +16,7 @@ public class CountryServiceImpl implements CountryService {
     public Country add(Country country) {
         return countryDao.add(country);
     }
+
     @Override
     public Country get(Long id) {
         Optional<Country> optionalCountry = countryDao.get(id);
