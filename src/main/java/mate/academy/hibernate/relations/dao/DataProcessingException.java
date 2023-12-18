96b598f436151ff3f8f13cb8c494dac9d827ece5
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-18 15:25:14 +0200

    added changes to the class

--- src/main/java/mate/academy/hibernate/relations/dao/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/dao/DataProcessingException.java
@@ -4,8 +4,4 @@ public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Throwable e) {
         super(message, e);
     }
-
-    public DataProcessingException(String message) {
-        super(message);
-    }
 }
