25569e28b16b72735f6856c1f9535804114fdb56
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-18 16:05:42 +0200

    added changes to the class ActorServiceImp

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,11 +1,9 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
-import mate.academy.hibernate.relations.dao.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
-
-import java.util.Optional;
 
 public class ActorServiceImpl implements ActorService {
     private final ActorDao actorDao;
