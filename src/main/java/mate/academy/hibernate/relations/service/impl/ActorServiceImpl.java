6fbf1e06da63d847e739d22d79ec48bbad19c62d
alukavenko <a.lukavenko@gmail.com>
2023-12-19 22:30:49 +0200

    Implemented Actor, Country, Movie relations

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,27 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
+    private static ActorDao dao;
+
+    public ActorServiceImpl(SessionFactory factory) {
+        dao = new ActorDaoImpl(factory);
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        return dao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        Optional<Actor> result = dao.get(id);
+        return result.orElseGet(Actor::new);
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,27 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
+import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
+    private static CountryDao dao;
+
+    public CountryServiceImpl(SessionFactory factory) {
+        dao = new CountryDaoImpl(factory);
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        return dao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        Optional<Country> result = dao.get(id);
+        return result.orElseGet(Country::new);
     }
 }
