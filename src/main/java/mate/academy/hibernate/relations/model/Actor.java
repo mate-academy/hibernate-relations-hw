9b49846d6e4323c8838655a7f0b54d4c3bbad18c
MateuszRostek <17rostek17@gmail.com>
2023-12-21 00:16:01 +0100

    hibernate dependencies, custom exception, models and HibernateUtil added

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,8 +1,14 @@
 package mate.academy.hibernate.relations.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Actor implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @ManyToOne
     private Country country;
 
     public Actor() {
