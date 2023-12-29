a1d32d55cafd732e29bd3f6f5751a9655cdaa384
Jakub Sekowski <sekowskijakub@gmail.com>
2023-12-29 14:54:48 +0100

    First solution of hibernate-relations-hw

--- src/main/java/mate/academy/hibernate/relations/exceptions/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/exceptions/DataProcessingException.java
@@ -1,2 +1,7 @@
-package mate.academy.hibernate.relations.exceptions;public class DataProcessingException {
+package mate.academy.hibernate.relations.exceptions;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
 }
