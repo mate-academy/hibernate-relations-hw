6a1bbabfe8ed5c5e4e95486000a59fd591f74a96
Olga Starkova <ole4kastar79@gmail.com>
2024-01-05 01:25:10 +0200

    CI Build errors fixed: CustomImportOrder

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,15 +1,15 @@
 package mate.academy.hibernate.relations.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "movie")
