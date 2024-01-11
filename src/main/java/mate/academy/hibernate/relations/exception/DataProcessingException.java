950afd3654d5d8aa665427dcbf60e7c1b91696f5
LeonIllya <dd0733567@gmail.com>
2024-01-11 21:38:46 +0200

    fix mistakes

--- src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -5,4 +5,8 @@ public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Throwable e) {
         super(message, e);
     }
+
+    public DataProcessingException(String message) {
+        super(message);
+    }
 }
