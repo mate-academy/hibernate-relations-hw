5087e7f17d45a0ba98d6c52facdebcdf9ffe60de
Hlib Bykovskyi <minexgleb@gmail.com>
2024-01-15 19:26:07 +0200

    Implement DAO, Service layers and Models

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public CountryDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,54 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Country add(Country country) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(country);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            throw new DataProcessingException("Failed to add " + country.toString()
+                    + " to database.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return country;
     }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        Country result = null;
+
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            result = session.get(Country.class, id);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            throw new DataProcessingException("Failed to get country with ID " + id
+                    + " from database.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        return Optional.ofNullable(result);
     }
 }
