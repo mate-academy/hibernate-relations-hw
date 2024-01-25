22a4c5c8fb3f530a0682a8daea691a4a25543d61
Anna Suprun <evgenevnaanna989@gmail.com>
2024-01-25 19:12:40 +0200

    implemented dao, services, added annotations to models, added dependencies to pom.xml, created hibernate.cfg.xml, exception

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,29 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
 public class ActorServiceImpl implements ActorService {
+    private final ActorDao dao;
+
+    public ActorServiceImpl(ActorDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        return dao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        Optional<Actor> actor = dao.get(id);
+        if (actor.isPresent()) {
+            return actor.get();
+        } else {
+            throw new RuntimeException("No actor with id=" + id + "is found.");
+        }
     }
 }
