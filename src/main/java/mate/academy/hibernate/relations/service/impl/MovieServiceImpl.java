9583b22296d0fabdfa196914f733f67cdcd45989
Andrii Mikhaylovskiy <Andrew.chase999@gmail.com>
2024-02-07 21:23:21 +0200

    task solution mark.1

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,29 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao dao;
+
+    public MovieServiceImpl(MovieDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return dao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        Optional<Movie> movie = dao.get(id);
+        if (movie.isPresent()) {
+            return movie.get();
+        } else {
+            throw new RuntimeException("No movie with id=" + id + "is found.");
+        }
     }
 }
