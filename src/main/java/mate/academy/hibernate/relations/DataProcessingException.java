1e24c6bb81b6c76d2229b7a8cefaafdfdc75976e
Bohdan <bohdan2704@gmail.com>
2024-02-10 18:38:20 +0200

    removed unused exception constructor, added enter at the end of hibernate conf file

--- src/main/java/mate/academy/hibernate/relations/DataProcessingException.java
+++ src/main/java/mate/academy/hibernate/relations/DataProcessingException.java
@@ -1,10 +1,6 @@
 package mate.academy.hibernate.relations;
 
 public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String msg) {
-        super(msg);
-    }
-
     public DataProcessingException(String msg, Exception e) {
         super(msg, e);
     }
