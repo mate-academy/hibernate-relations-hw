315512ef289a8566ffa2f674c81b653a9e8023d2
Kacper <kacperkukula97@gmail.com>
2024-02-26 19:19:20 +0100

    Changed deprecated method save() to persist()

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -21,7 +21,7 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.save(movie);
+            session.persist(movie);
             transaction.commit();
         } catch (Exception ex) {
             if (transaction != null) {
