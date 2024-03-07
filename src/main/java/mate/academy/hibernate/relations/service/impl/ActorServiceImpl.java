6a9fdb114c7dfc3c11f902f5b57c33637706a727
MilianCode <milian.code@gmail.com>
2024-03-07 13:38:42 +0100

    Added solution to hibernate-relations-hw

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,28 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
+
+    private SessionFactory sessionFactory;
+
+    public ActorServiceImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        ActorDao actorDao = new ActorDaoImpl(sessionFactory);
+        return actorDao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        ActorDao actorDao = new ActorDaoImpl(sessionFactory);
+        return actorDao.get(id).get();
     }
 }
