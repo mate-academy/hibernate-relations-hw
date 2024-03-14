c634e11e5ccf1bc7ecddf5d91c9f6bdcfc96aa28
Yana Stepanova <yanna.stepanova@gmail.com>
2024-03-14 17:04:44 +0200

    added solution to the task

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public CountryDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,43 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Country add(Country country) {
-        return null;
+        if (country == null) {
+            throw new RuntimeException("Link to the country is null");
+        }
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(country);
+            transaction.commit();
+            if (country.getId() == null) {
+                throw new RuntimeException("Can't add this country to db - " + country);
+            }
+        } catch (RuntimeException ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Exception", ex);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return country;
     }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        if (id == null) {
+            throw new RuntimeException("This 'id' is null");
+        }
+        Country country;
+        try (Session session = factory.openSession()) {
+            country = session.get(Country.class, id);
+        } catch (RuntimeException ex) {
+            throw new DataProcessingException("Can't get country from db by id = " + id, ex);
+        }
+        return Optional.ofNullable(country);
     }
 }
--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MovieDaoImpl extends AbstractDao implements MovieDao {
     public MovieDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,43 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
 
     @Override
     public Movie add(Movie movie) {
-        return null;
+        if (movie == null) {
+            throw new RuntimeException("Link to the movie is null");
+        }
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+            if (movie.getId() == null) {
+                throw new RuntimeException("Can't add this movie to db -" + movie);
+            }
+        } catch (RuntimeException ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Exception", ex);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return movie;
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return null;
+        if (id == null) {
+            throw new RuntimeException("This 'id' is null");
+        }
+        Movie movie;
+        try (Session session = factory.openSession()) {
+            movie = session.get(Movie.class, id);
+        } catch (RuntimeException ex) {
+            throw new DataProcessingException("Can't get movie from db by id = " + id, ex);
+        }
+        return Optional.ofNullable(movie);
     }
 }
