24bd9db59e73f80e973b3d387db7cdfc91befd35
Oleksand Kolisnyk <kolisnykol@gmail.com>
2024-03-24 18:32:34 +0200

    Hibernate relations solution

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,32 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
+import mate.academy.hibernate.relations.lib.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
-    @Override
+    private ActorDao actorDao;
+    private SessionFactory sessionFactory;
+
+    public ActorServiceImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+        actorDao = new ActorDaoImpl(sessionFactory);
+    }
+
     public Actor add(Actor actor) {
-        return null;
+        return actorDao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        Optional<Actor> actorOptional = actorDao.get(id);
+        if (actorOptional.isPresent()) {
+            return actorOptional.get();
+        }
+        throw new DataProcessingException("Not found in DB actor with id = " + id);
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,32 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
+import mate.academy.hibernate.relations.lib.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
+import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
-    @Override
+    private CountryDao countryDao;
+    private SessionFactory sessionFactory;
+
+    public CountryServiceImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+        countryDao = new CountryDaoImpl(sessionFactory);
+    }
+
     public Country add(Country country) {
-        return null;
+        return countryDao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        Optional<Country> countryOptional = countryDao.get(id);
+        if (countryOptional.isPresent()) {
+            return countryOptional.get();
+        }
+        throw new DataProcessingException("Not found in DB country with id = " + id);
     }
 }
