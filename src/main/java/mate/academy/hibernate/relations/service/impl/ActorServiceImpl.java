c871a3592f396d0cc706c6a24f846ffb942a07a9
Oleksand Kolisnyk <kolisnykol@gmail.com>
2024-03-26 21:05:31 +0200

    Fixed DataProcessingException, *ServiceImpl, Main
    DataProcessingException - removed redundant constructor
    *ServiceImpl - replaced sessionFactory to *DaoIml in  parameters and constructors
    Main - changed initialization *Services

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -2,18 +2,14 @@ package mate.academy.hibernate.relations.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.ActorDao;
-import mate.academy.hibernate.relations.dao.impl.ActorDaoImpl;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
-import org.hibernate.SessionFactory;
 
 public class ActorServiceImpl implements ActorService {
     private ActorDao actorDao;
-    private SessionFactory sessionFactory;
 
-    public ActorServiceImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-        actorDao = new ActorDaoImpl(sessionFactory);
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
     }
 
     public Actor add(Actor actor) {
--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -2,18 +2,14 @@ package mate.academy.hibernate.relations.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
-import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
     private CountryDao countryDao;
-    private SessionFactory sessionFactory;
 
-    public CountryServiceImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-        countryDao = new CountryDaoImpl(sessionFactory);
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
     }
 
     public Country add(Country country) {
