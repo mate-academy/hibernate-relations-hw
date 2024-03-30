78474f26f02d073fba16ddc190fd2c11ac5674df
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-03-30 13:03:19 +0200

    implemented dao classes and DataProcessingException

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -2,21 +2,49 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
+    private static final String ADD_EXCEPTION_MESSAGE = "Failed to add country to DB ";
+    private static final String GET_EXCEPTION_MESSAGE = "Failed to get country from DB ";
+
     public CountryDaoImpl(SessionFactory sessionFactory) {
         super(sessionFactory);
     }
 
     @Override
     public Country add(Country country) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(country);
+            transaction.commit();
+        } catch (HibernateException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException(ADD_EXCEPTION_MESSAGE + country);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
         return null;
     }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            return Optional.ofNullable(session.get(Country.class, id));
+        } catch (HibernateException e) {
+            throw new DataProcessingException(GET_EXCEPTION_MESSAGE + id);
+        }
     }
 }
