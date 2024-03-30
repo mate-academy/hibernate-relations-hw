9272e2a2026c8f56704fc16880d688fa9b5d730e
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-03-30 12:15:22 +0200

    codestyle fix

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -8,9 +8,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-
 import java.util.ArrayList;
 import java.util.List;
+
 @Entity
 @Table(name = "movies")
 public class Movie implements Cloneable {
