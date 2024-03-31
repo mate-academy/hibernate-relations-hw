4f1fbdfca68a9b08f68f37cb16f26e2e0ce6339f
VolandevlodD <vladradko2000@gmail.com>
2024-03-31 19:30:12 +0300

    Implemented Hibernate relations structure with DAO implementations, models, and services.

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,27 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.NoSuchElementException;
+import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao dao;
+
+    public MovieServiceImpl(MovieDao dao) {
+        this.dao = dao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return dao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return dao.get(id).orElseThrow(() ->
+                new NoSuchElementException(
+                        String.format(
+                                "Movie with ID %d does not exist in the " + "database.", id)));
     }
 }
