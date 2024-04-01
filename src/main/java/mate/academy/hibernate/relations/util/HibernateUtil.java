a9da8dd8396a3c0ce1897487a32d920c8ef1261a
Pavlo Betsa <bigworld2303@gmail.com>
2024-04-01 17:06:40 +0300

    hibernate-relations-solution V1.0

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory instance = getInstance();
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
+    }
+
+    private static SessionFactory getInstance() {
+        return new Configuration().configure().buildSessionFactory();
     }
 }
