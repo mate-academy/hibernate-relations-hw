b33ca53f8347883a02a079ab001ce82b82516b11
TonyH277 <robhobs50@gmail.com>
2024-04-01 16:10:21 +0300

    change instance to lower case

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -4,10 +4,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory INSTANCE = initializeFactory();
+    private static SessionFactory instance = initializeFactory();
 
     public static SessionFactory getSessionFactory() {
-        return INSTANCE;
+        return instance;
     }
 
     private static SessionFactory initializeFactory() {
