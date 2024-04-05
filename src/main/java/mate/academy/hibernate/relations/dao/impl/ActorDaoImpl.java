6a409a8fc7a3c3f22efd1cf418327725264b936e
Danylo Kishchenko <amfich@gmail.com>
2024-04-05 22:47:09 +0300

    - added empty line in the end of `hibernate.cfg.xml` file - changed DataProcessingException to EntityNotFoundException in *ServiceImpl classes

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -1,8 +1,8 @@
 package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
-import mate.academy.hibernate.relations.dao.DataProcessingException;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
