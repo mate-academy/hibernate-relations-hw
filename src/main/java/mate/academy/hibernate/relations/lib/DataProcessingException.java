ce186e21642575c0c53db603dfde1c4fcc7cf9a3
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-04-30 16:48:16 +0300

    solve the task

---
+++ src/main/java/mate/academy/hibernate/relations/lib/DataProcessingException.java
@@ -0,0 +1,9 @@
+package mate.academy.hibernate.relations.lib;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException() {
+    }
+
+    public DataProcessingException(String message, Exception e) {
+    }
+}
