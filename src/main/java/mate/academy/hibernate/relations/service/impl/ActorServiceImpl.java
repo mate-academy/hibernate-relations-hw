5dbfcdf699d58c89b2c2012874efe4d5e75d3a93
nastasiiaKulinich <kussik2004@gmail.com>
2024-05-02 08:24:53 +0300

    used method orelsethrow instead of get method in services in get methods

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
@@ -18,6 +19,7 @@ public class ActorServiceImpl implements ActorService {
 
     @Override
     public Actor get(Long id) {
-        return actorDao.get(id).get();
+        return actorDao.get(id)
+                .orElseThrow(() -> new EntityNotFoundException("Actor not found with id: " + id));
     }
 }
