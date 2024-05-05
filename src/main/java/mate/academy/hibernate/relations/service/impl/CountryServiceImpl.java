3e6d96c9123d5db70e45caef26162185ecd8a855
Vladlen Soloviov <retkinf@gmail.com>
2024-05-05 16:12:12 +0300

    changed .get() to .orElseThrow()

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
@@ -23,6 +24,7 @@ public class CountryServiceImpl implements CountryService {
 
     @Override
     public Country get(Long id) {
-        return countryDao.get(id).get();
+        return countryDao.get(id)
+                .orElseThrow(() -> new EntityNotFoundException("Not found country with id: " + id));
     }
 }
