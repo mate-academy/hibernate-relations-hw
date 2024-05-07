2e69e1e82f540892277c98095423e04826088ae2
petro feniak <petro.fenyak@gmail.com>
2024-05-07 18:52:18 +0300

    changed method save() on persist()

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -21,7 +21,7 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.save(country);
+            session.persist(country);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
