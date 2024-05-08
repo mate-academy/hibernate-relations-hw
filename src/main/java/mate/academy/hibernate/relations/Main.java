62259e8d6c4509a44410b632827c63e74cb5338f
Vadym Stanishevskyi <mainvadym@gmail.com>
2024-05-08 09:53:22 +0200

    added fixes after review

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -15,24 +15,23 @@ import org.hibernate.SessionFactory;
 
 public class Main {
     public static void main(String[] args) {
-        // use this session factory when you will initialize service instances
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
         Country usa = new Country("USA");
         CountryService countryService
-                = new CountryServiceImpl(sessionFactory); // TODO: initialize this instance
+                = new CountryServiceImpl(sessionFactory);
         countryService.add(usa);
 
         Actor vinDiesel = new Actor("Vin Diesel");
         vinDiesel.setCountry(usa);
         ActorService actorService
-                = new ActorServiceImpl(sessionFactory); // TODO: initialize this instance
+                = new ActorServiceImpl(sessionFactory);
         actorService.add(vinDiesel);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setActors(List.of(vinDiesel));
         MovieService movieService
-                = new MovieServiceImpl(sessionFactory); // TODO: initialize this instance
+                = new MovieServiceImpl(sessionFactory);
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
     }
