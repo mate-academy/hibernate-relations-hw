4dc106919ef8afdc47c752caf4a40b608bfefc8d
Kinereiko <kinereiko@gmail.com>
2024-05-09 11:40:36 +0300

    Error at Country Entity

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,32 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        SessionFactory sessionFactory = factory;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(actor);
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            throw new DataProcessingException("Can't add actor: " + actor, ex);
+        } finally {
+            session.close();
+        }
+        return actor;
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        Optional<Actor> actor;
+        try (SessionFactory sessionFactory = factory;
+                Session session = sessionFactory.openSession()) {
+            actor = Optional.ofNullable(session.get(Actor.class, id));
+        } catch (Exception ex) {
+            throw new DataProcessingException("Can't find actor with id: " + id, ex);
+        }
+        return actor;
     }
 }
--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public CountryDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,31 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Country add(Country country) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(country);
+            transaction.commit();
+        } catch (Exception ex) {
+            transaction.rollback();
+            throw new DataProcessingException("Can't add country: " + country, ex);
+        } finally {
+            session.close();
+        }
+        return country;
     }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        Optional<Country> country;
+        try (SessionFactory sessionFactory = factory;
+                Session session = sessionFactory.openSession()) {
+            country = Optional.ofNullable(session.get(Country.class, id));
+        } catch (Exception ex) {
+            throw new DataProcessingException("Can't find country with id: " + id, ex);
+        }
+        return country;
     }
 }
