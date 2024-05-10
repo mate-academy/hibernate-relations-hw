53470b69bf534e60bb4c1615159560a15f0d2a32
Vlad <mazafackery@gmail.com>
2024-05-10 20:26:17 +0300

    change actor-movie relations

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -2,12 +2,7 @@ package mate.academy.hibernate.relations.model;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "movies")
@@ -17,7 +12,7 @@ public class Movie implements Cloneable {
     private Long id;
     private String title;
 
-    @OneToMany
+    @ManyToMany
     private List<Actor> actors;
 
     public Movie() {
