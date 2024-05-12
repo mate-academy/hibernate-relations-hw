7f68367094b1dc212c37a26b574d6ae5c8d93c35
Dima <dmmhnk23@gmail.com>
2024-05-12 12:12:41 +0200

    fixed mistake

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,11 +1,11 @@
 package mate.academy.hibernate.relations.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
