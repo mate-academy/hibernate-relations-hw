b7f410b775600adb23c9d8d7b26a2c30a908e1f3
Dima <dmmhnk23@gmail.com>
2024-05-13 17:56:11 +0200

    corrected comments

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -7,8 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.Table;
 import javax.persistence.JoinTable;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "movies")
