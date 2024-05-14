a775a30a8723380061732378d912ce8fa03e6735
NekitCool1 <gnnikita97@gmail.com>
2024-05-14 17:26:30 +0300

    The task has fixed after comment #2

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -20,7 +20,7 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
         try {
             session = factory.openSession();
             transaction = session.beginTransaction();
-            session.save(actor);
+            session.persist(actor);
             transaction.commit();
         } catch (RuntimeException e) {
             if (transaction != null) {
