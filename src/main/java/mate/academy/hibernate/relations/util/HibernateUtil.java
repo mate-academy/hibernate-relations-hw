65fea19e778800ece1192a923f37c8c64066cd7b
maksym_korniev <maksymkorniev88@gmail.com>
2024-05-28 21:45:40 +0300

    added required hibernate dependencies complete implementation of all classes in the mate/academy/hibernate/relations/model package completed implementation of all classes in the mate/academy/hibernate/relations/dao/impl package created your custom unchecked DataProcessingException and throw it in the catch block on the DAO layer completed implementation of all classes in the mate/academy/hibernate/relations/service/impl/ package created a hibernate.cfg.xml file and implement mate/academy/hibernate/relations/util/HibernateUtil class (this is required for the next step) made mate/academy/Main.java work (you should be able to run main() method without any errors)

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,21 @@
 package mate.academy.hibernate.relations.util;
 
+import mate.academy.hibernate.relations.excpetion.DataProcessingException;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory SESSION_FACTORY = initSessionFactory();
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't create session factory ", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return SESSION_FACTORY;
     }
 }
