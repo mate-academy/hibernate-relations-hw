f525cb204113d71f1a7f1a851aa839fa504b309c
Oleksii Bashkirov <bashkirov.o.u@ukr.net>
2024-05-28 16:16:00 +0300

    changed RTE to EntityNotFoundException(); removed empty line;

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -5,7 +5,6 @@ import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
 public class CountryServiceImpl implements CountryService {
-
     private final CountryDao countryDao;
 
     public CountryServiceImpl(CountryDao countryDao) {
