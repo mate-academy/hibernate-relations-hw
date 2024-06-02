48911ee7d7234f228b8ca9ce7882ae6b1e89b9a6
Yurii <wervmer>
2024-06-02 11:41:41 +0300

    added main and HibernateUtil classes

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -3,9 +3,7 @@ package mate.academy.hibernate.relations.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
 public class HibernateUtil {
-
     private static final SessionFactory sessionFactory = initSessionFactory();
 
     private static SessionFactory initSessionFactory() {
