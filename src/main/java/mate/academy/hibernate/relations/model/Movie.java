cd92bfca09c11db243d5d5b9a2d25a6287661256
Artem Klishch <artemklishch@gmail.com>
2024-06-04 18:49:25 +0300

    fix: fixes

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,13 +1,12 @@
 package mate.academy.hibernate.relations.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +19,7 @@ public class Movie implements Cloneable {
     private Long id;
     private String title;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    @ManyToMany
     @JoinTable(
             name = "movies_actors",
             joinColumns = @JoinColumn(name = "movie_id"),
