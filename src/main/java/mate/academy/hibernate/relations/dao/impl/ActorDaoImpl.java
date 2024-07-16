2cb6d4c87464810191766be5f3512c39a368835e
Yuliia <innishastudy@gmail.com>
2024-07-16 16:48:43 +0300

    configured hibernate and completed all implementations

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,11 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,40 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        Long id;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(actor);
+            transaction.commit();
+            id = (Long) session.getIdentifier(actor);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't save actor to DB: " + actor, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        actor.setId(id);
+        return actor;
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        try (Session session = factory.openSession()) {
+            Actor actor = session.get(Actor.class, id);
+            if (actor != null) {
+                return Optional.of(actor);
+            }
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get actor from DB by id: " + id, e);
+
+        }
+        return Optional.empty();
     }
 }
