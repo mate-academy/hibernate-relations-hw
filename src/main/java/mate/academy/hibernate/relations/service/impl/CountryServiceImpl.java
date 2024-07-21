20eabadeab0ecaff879ddc794cfa0fcde18be9b6
Illya Pantazi <illios.piv@gmail.com>
2024-07-21 02:13:46 +0200

    Added required hibernate dependencies. Completed implementation of all classes in the mate/academy/hibernate/relations/model package. Completed implementation of all classes in the mate/academy/hibernate/relations/dao/impl package. Created my custom unchecked DataProcessingException. Completed implementation of all classes in the mate/academy/hibernate/relations/service/impl/ package. Created a hibernate.cfg.xml file and implement mate/academy/hibernate/relations/util/HibernateUtil class. Edited mate/academy/Main.java

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,31 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
 public class CountryServiceImpl implements CountryService {
+    private final CountryDao countryDao;
+
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        if (country == null) {
+            throw new DataProcessingException("The argument (country) is null");
+        }
+        return countryDao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        if (id == null) {
+            throw new DataProcessingException("The argument (id) is null");
+        }
+        return countryDao.get(id).orElseThrow(()
+                -> new DataProcessingException("Can't get country by id, country is null"));
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,31 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        if (movie == null) {
+            throw new DataProcessingException("The argument (movie) is null");
+        }
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        if (id == null) {
+            throw new DataProcessingException("The argument (id) is null");
+        }
+        return movieDao.get(id).orElseThrow(()
+                -> new DataProcessingException("Can't get movie by id, movie is null"));
     }
 }
