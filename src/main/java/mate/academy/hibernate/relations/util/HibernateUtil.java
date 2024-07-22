8e1e30b17fe6b7326c2525eb74a439583ed602e8
ChabVlad <vlad.chabak@gmail.com>
2024-07-22 13:09:58 +0100

    test

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,19 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory instance = initSessionFactory();
+
+    private static SessionFactory initSessionFactory() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
+    public HibernateUtil() {
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return instance;
     }
 }
