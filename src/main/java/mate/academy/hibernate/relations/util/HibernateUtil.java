fbbe90de97d4959b23e5282ff5a522d3c7f1ddc7
Greshch <greshchook@gmail.com>
2024-08-06 11:09:18 +0300

    all the test were passed

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.util;
 
+import lombok.Getter;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    public static SessionFactory getSessionFactory() {
-        return null;
+    @Getter
+    private static final SessionFactory sessionFactory;
+
+    static {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        sessionFactory = configuration.buildSessionFactory();
     }
 }
