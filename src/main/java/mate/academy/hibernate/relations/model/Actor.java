e203a8abf3eea37d1cbc2fdee39e20fbf7bbc466
Greshch <greshchook@gmail.com>
2024-08-09 16:06:52 +0300

    fix names of entities

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Setter
 @Getter
 @Entity
+@Table(name = "actors")
 @AllArgsConstructor
 public class Actor implements Cloneable {
     @Id
