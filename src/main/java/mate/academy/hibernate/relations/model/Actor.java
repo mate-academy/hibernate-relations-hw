bc96dab029d287ba0d823e3238eb371671138b8c
foxstudent106239 <mykhailo.kornukh@gmail.com>
2024-08-22 23:15:59 +0300

    full

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,8 +1,23 @@
 package mate.academy.hibernate.relations.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "actors")
 public class Actor implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
+    @OneToOne(fetch = FetchType.EAGER)
     private Country country;
 
     public Actor() {
@@ -52,9 +67,14 @@ public class Actor implements Cloneable {
     @Override
     public String toString() {
         return "Actor{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + ", country='" + country + '\''
+                + "id="
+                + id
+                + ", name='"
+                + name
+                + '\''
+                + ", country='"
+                + country
+                + '\''
                 + '}';
     }
 }
