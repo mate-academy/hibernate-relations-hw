5f3f08610a9df511ef192facab3f93b7097db688
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-09-10 12:30:08 +0300

    The thrown exception is changed to EntityNotFoundException.

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.ActorDao;
-import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
@@ -20,6 +20,6 @@ public class ActorServiceImpl implements ActorService {
     @Override
     public Actor get(Long id) {
         return actorDao.get(id).orElseThrow(() ->
-                new DataProcessingException(String.format("Cannot find actor with id:%d", id)));
+                new EntityNotFoundException(String.format("Cannot find actor with id:%d", id)));
     }
 }
