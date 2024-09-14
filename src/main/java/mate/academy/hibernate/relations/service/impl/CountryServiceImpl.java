d090a2c25cbbfd9eaa63ecfdbc681cd727e24ecb
Ihor Panchenko <ihpanchenko@gmail.com>
2024-09-14 21:32:41 +0300

    Fixed mistakes with exceptions.

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import javax.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
@@ -20,6 +20,6 @@ public class CountryServiceImpl implements CountryService {
     @Override
     public Country get(Long id) {
         return countryDao.get(id).orElseThrow(
-                () -> new DataProcessingException("No country found with id: " + id));
+                () -> new EntityNotFoundException("No country found with id: " + id));
     }
 }
