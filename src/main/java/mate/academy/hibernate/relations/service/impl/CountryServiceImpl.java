ca392377d50d58907203f6e5104c467ad80c4d1c
Vlad Nadtochiy <v20102002n@gmail.com>
2024-09-16 16:00:45 +0300

    changes were made 2

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.exceptions.DataProcessingException;
+import mate.academy.hibernate.relations.exceptions.EntityNotFoundException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
@@ -20,6 +20,6 @@ public class CountryServiceImpl implements CountryService {
     @Override
     public Country get(Long id) {
         return countryDao.get(id).orElseThrow(
-                () -> new DataProcessingException("Can't get country by id " + id));
+                () -> new EntityNotFoundException("Can't get country by id " + id));
     }
 }
