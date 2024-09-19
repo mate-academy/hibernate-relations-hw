b712c40f732967a5efff4cb3c3c3c5f1d299d21d
flamehost <flamehost@syneforge.com>
2024-09-19 17:42:27 +0300

    edits by comments

--- src/main/java/mate/academy/hibernate/relations/exception/MovieNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.hibernate.relations.exception;
-
-public class MovieNotFoundException extends RuntimeException {
-    public MovieNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,7 +1,7 @@
 package mate.academy.hibernate.relations.service.impl;
 
 import mate.academy.hibernate.relations.dao.CountryDao;
-import mate.academy.hibernate.relations.exception.MovieNotFoundException;
+import mate.academy.hibernate.relations.exception.EntityNotFoundException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
@@ -20,6 +20,6 @@ public class CountryServiceImpl implements CountryService {
     @Override
     public Country get(Long id) {
         return countryDao.get(id).orElseThrow(()
-                        -> new MovieNotFoundException("Country id:" + id + " not found"));
+                        -> new EntityNotFoundException("Country id:" + id + " not found"));
     }
 }
