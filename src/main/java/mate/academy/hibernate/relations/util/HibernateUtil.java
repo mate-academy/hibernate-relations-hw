0cd5235d880f6277e693097433a5515f10cf4913
Igor Gunder <gunderigorv@gmail.com>
2024-10-24 00:37:57 +0300

    The task is done! I hope it's correct!

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,29 @@
 package mate.academy.hibernate.relations.util;
 
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static SessionFactory sessionFactory = initSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            Configuration configuration = new Configuration().configure();
+            configuration.setProperty("hibernate.connection.username",
+                    System.getenv("DB_USER"));
+            configuration.setProperty("hibernate.connection.password",
+                    System.getenv("DB_PASSWORD"));
+            return configuration.buildSessionFactory();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t create session factory", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return sessionFactory;
     }
 }
