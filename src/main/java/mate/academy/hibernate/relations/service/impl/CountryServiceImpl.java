bacd32c480e82f7d7bccd3b7933d7909c39e6da0
Bohdan Stratiychuk <vasyahunta@gmail.com>
2024-10-28 15:14:23 +0200

    implemented Dao and Service

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,30 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
 public class CountryServiceImpl implements CountryService {
+    private final CountryDao abstractDao;
+
+    public CountryServiceImpl(CountryDaoImpl countryDao) {
+        this.abstractDao = countryDao;
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        return abstractDao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        Optional<Country> movie = abstractDao.get(id);
+        if (movie.isEmpty()) {
+            throw new DataProcessingException("Country not found with id: " + id);
+        }
+        return movie.get();
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,29 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao abstractDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.abstractDao = movieDao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        return abstractDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        Optional<Movie> movie = abstractDao.get(id);
+        if (movie.isEmpty()) {
+            throw new DataProcessingException("Movie not found with id: " + id);
+        }
+        return movie.get();
     }
 }
