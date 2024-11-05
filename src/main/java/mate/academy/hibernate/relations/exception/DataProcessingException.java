60048ae5d29ccd14163d059f5d888148256465a4
Nataliya Plastun <natalya21753@gmail.com>
2024-11-05 20:19:24 +0200

    commit relations

---
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -0,0 +1,13 @@
+package mate.academy.hibernate.relations.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+
--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,42 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ActorServiceImpl implements ActorService {
+    private static final Logger logger = LogManager.getLogger(ActorServiceImpl.class);
+    private final ActorDao actorDao;
+
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        try {
+            return actorDao.add(actor);
+        } catch (Exception e) {
+            logger.error("Can't add actor: " + actor, e);
+            throw new DataProcessingException("Can't add actor: " + actor, e);
+        }
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        try {
+            Optional<Actor> actorOptional = actorDao.get(id);
+            if (actorOptional.isEmpty()) {
+                throw new DataProcessingException("Actor not found with id: " + id);
+            }
+            return actorOptional.get();
+        } catch (Exception e) {
+            logger.error("Can't get actor by id: " + id, e);
+            throw new DataProcessingException("Can't get actor by id: " + id, e);
+        }
     }
 }
