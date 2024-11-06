ff8a19154b34af782e35611596f1a492a513fafa
Xoteki <xotekijke@gmail.com>
2024-11-06 20:07:13 +0200

    solution added

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-
 import java.util.ArrayList;
 import java.util.List;
 
