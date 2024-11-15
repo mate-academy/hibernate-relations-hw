d8756421dfcecb8173aff17adfaf87e171e1bb9e
StoneBlood <brkalix@gmail.com>
2024-11-15 10:34:27 +0200

    implemented methods `get` and `add` in classes ActorDaoImpl, CountryDaoImpl, MovieDaoImpl

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,12 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import mate.academy.hibernate.relations.util.HibernateUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,43 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(actor);
+            transaction.commit();
+            return actor;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can not add actor: " + actor, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+
+        try {
+            session = sessionFactory.openSession();
+            Actor actor = session.get(Actor.class, id);
+            return Optional.ofNullable(actor);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not get actor by id: " + id, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
