243b5faed85f0ad7d9dd6dc28766d0eb850f2d72
fwhoami <clahfclns@gmail.com>
2024-11-18 04:59:14 +0200

    solution: hibernate relations

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,37 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
 public class ActorServiceImpl implements ActorService {
+    private final ActorDao actorDao;
+
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        try {
+            return actorDao.add(actor);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't add actor: " + actor, e);
+        }
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        try {
+            Optional<Actor> actorOptional = actorDao.get(id);
+            if (actorOptional.isEmpty()) {
+                throw new DataProcessingException("Actor not found with id: " + id);
+            }
+            return actorOptional.get();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get actor by id: " + id, e);
+        }
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,38 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
 public class CountryServiceImpl implements CountryService {
+    private final CountryDao countryDao;
+
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        try {
+            return countryDao.add(country);
+        } catch (Exception e) {
+            throw new DataProcessingException(
+                    "Unexpected error while adding country: " + country, e);
+        }
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        try {
+            Optional<Country> countryOptional = countryDao.get(id);
+            if (countryOptional.isEmpty()) {
+                throw new DataProcessingException("Country not found with id: " + id);
+            }
+            return countryOptional.get();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get country by id: " + id, e);
+        }
     }
 }
