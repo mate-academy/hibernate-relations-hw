861ad829818c825f7c97493a794ba8288f305439
Ivan Synenko <ivan.synenko@nure.ua>
2024-11-22 02:26:25 +0200

    Fixed import order

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -1,9 +1,9 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import java.util.Optional;
 import mate.academy.hibernate.relations.DataProcessingException;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
-import java.util.Optional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
