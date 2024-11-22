3327ab0d8207ba59fb7886bce73c98e5f4deaac8
Ivan Synenko <ivan.synenko@nure.ua>
2024-11-22 02:16:56 +0200

    All tests do pass now

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -1,9 +1,12 @@
 package mate.academy.hibernate.relations.dao.impl;
 
-import java.util.Optional;
+import mate.academy.hibernate.relations.DataProcessingException;
 import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import java.util.Optional;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,36 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(actor);
+            transaction.commit();
+            return actor;
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Failed to save the actor. " + ex.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        Session session;
+        try {
+            session = factory.openSession();
+            Optional<Actor> result = Optional.ofNullable(session.get(Actor.class, id));
+            session.close();
+            return result;
+        } catch (Exception ex) {
+            throw new DataProcessingException("Failed to create a session" + ex.getMessage());
+        }
     }
 }
--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -1,9 +1,13 @@
 package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
+
+import mate.academy.hibernate.relations.DataProcessingException;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public CountryDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,36 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Country add(Country country) {
-        return null;
-    }
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(country);
+            transaction.commit();
+            return country;
+        } catch (Exception ex) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Failed to save the country to the database. "
+                    + ex.getMessage());
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }    }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        Session session;
+        try {
+            session = factory.openSession();
+            Optional<Country> result = Optional.ofNullable(session.get(Country.class, id));
+            session.close();
+            return result;
+        } catch (Exception ex) {
+            throw new DataProcessingException("Failed to create a session" + ex.getMessage());
+        }
     }
 }
