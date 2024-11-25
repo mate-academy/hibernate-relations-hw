2a6e8274f9c9c47fd174e1e073c67bd4780efcf3
Julianevinska <Julianevinska@gmail.com>
2024-11-26 00:49:41 +0100

    added solution

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,31 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+
+    private static final SessionFactory SESSION_FACTORY = buildSessionFactory();
+
+    private HibernateUtil() {
+    }
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return SESSION_FACTORY;
+    }
+
+    public static void shutdown() {
+        if (SESSION_FACTORY != null) {
+            SESSION_FACTORY.close();
+        }
     }
 }
