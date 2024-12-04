2d9291654ba446dc4b3e006299359f9f10bc4da0
d.nosenko <daniil.nosenkoo@gmail.com>
2024-12-04 01:05:21 +0100

    add required hibernate dependencies complete implementation of all classes in the mate/academy/hibernate/relations/model package complete implementation of all classes in the mate/academy/hibernate/relations/dao/impl package create custom unchecked DataProcessingException and throw it in the catch block on the DAO layer complete implementation of all classes in the mate/academy/hibernate/relations/service/impl/ package create a hibernate.cfg.xml file and implement mate/academy/hibernate/relations/util/HibernateUtil class make mate/academy/Main.java work

--- src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/ActorDaoImpl.java
@@ -2,8 +2,12 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Actor;
+import mate.academy.hibernate.relations.util.HibernateUtil;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class ActorDaoImpl extends AbstractDao implements ActorDao {
     public ActorDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +16,38 @@ public class ActorDaoImpl extends AbstractDao implements ActorDao {
 
     @Override
     public Actor add(Actor actor) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(actor);
+            transaction.commit();
+            return actor;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Cannot ADD actor from Database " + actor, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public Optional<Actor> get(Long id) {
-        return null;
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            Actor actor = session.get(Actor.class, id);
+            return Optional.of(actor);
+        } catch (Exception e) {
+            throw new DataProcessingException("Cannot GET actor from Database " + id, e);
+        }
     }
 }
