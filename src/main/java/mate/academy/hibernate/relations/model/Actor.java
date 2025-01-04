fc4f38816a27bba62b08f46bb5880150defe404c
Alinka Lyn <alinka.lyn@ukr.net>
2025-01-04 21:07:19 +0200

    solved task

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,9 +1,9 @@
 package mate.academy.hibernate.relations.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
 
--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,12 +1,12 @@
 package mate.academy.hibernate.relations.model;
 
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.JoinTable;
 import javax.persistence.JoinColumn;
 
