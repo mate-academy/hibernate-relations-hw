757dd090722ac731cb99fb000092057d7a3abc4d
Alinka Lyn <alinka.lyn@ukr.net>
2025-01-04 21:12:36 +0200

    solved task

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -2,9 +2,9 @@ package mate.academy.hibernate.relations.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
