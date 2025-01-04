dbfc4eb4842a05838d54bfd7872c4efb46ddd6b1
Alinka Lyn <alinka.lyn@ukr.net>
2025-01-04 21:14:56 +0200

    solved task

--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -1,8 +1,8 @@
 package mate.academy.hibernate.relations.model;
 
-import javax.persistence.Entity;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
