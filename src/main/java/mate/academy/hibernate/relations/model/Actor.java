62e229a27b86dc186d94fb08508bceb65b6e0263
Alinka Lyn <alinka.lyn@ukr.net>
2025-01-04 21:02:07 +0200

    solved task

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -1,11 +1,13 @@
 package mate.academy.hibernate.relations.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
 
+@Entity
 public class Actor implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/mate/academy/hibernate/relations/model/Movie.java
+++ src/main/java/mate/academy/hibernate/relations/model/Movie.java
@@ -2,13 +2,15 @@ package mate.academy.hibernate.relations.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
 
+@Entity
 public class Movie implements Cloneable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
