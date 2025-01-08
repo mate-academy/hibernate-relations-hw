6c7407d9ad502735e590f0e3fa16b45eb5f91c6c
oKukHtina <olakukhtina95@gmail.com>
2025-01-09 01:05:56 +0200

    added dependencies completed all implementations created unchecked exception

---
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -0,0 +1,10 @@
+package mate.academy.hibernate.relations.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException() {
+    }
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,39 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
 public class CountryServiceImpl implements CountryService {
+    public static final String COUNTRY_NULL_EXCEPTION_MESSAGE = "Country can't be null";
+    public static final String ID_NULL_EXCEPTION_MESSAGE = "Id can't be null";
+    public static final String COUNTRY_NOT_FOUND_MESSAGE = "Country with id: %d was not found.";
+    private CountryDao countryDao;
+
+    public CountryServiceImpl(CountryDao countryDao) {
+        this.countryDao = countryDao;
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        if (country == null) {
+            throw new RuntimeException(COUNTRY_NULL_EXCEPTION_MESSAGE);
+        }
+        countryDao.add(country);
+        return country;
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException(ID_NULL_EXCEPTION_MESSAGE);
+        }
+
+        return countryDao.get(id)
+                .orElseThrow(
+                        () -> new IllegalArgumentException(
+                                String.format(COUNTRY_NOT_FOUND_MESSAGE, id)
+                        )
+                );
     }
 }
