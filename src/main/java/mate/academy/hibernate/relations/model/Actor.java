2fc78a5f08da76eb36c443f0729ce24bbde2c043
Teslenko Daniil <burgalhashem@gmail.com>
2025-01-31 01:16:04 +0200

    little fix

--- src/main/java/mate/academy/hibernate/relations/model/Actor.java
+++ src/main/java/mate/academy/hibernate/relations/model/Actor.java
@@ -8,7 +8,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
-
 @Entity
 @Table(name = "actors")
 public class Actor implements Cloneable {
