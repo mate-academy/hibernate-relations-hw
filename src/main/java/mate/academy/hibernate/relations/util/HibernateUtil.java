a2a863790133bcd50862dfaaee0e142ac6f153b8
Loiev Roman <robolito0420@gmail.com>
2025-02-08 00:49:42 +0200

    Task is done. Relations have been configured.

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,16 @@
 package mate.academy.hibernate.relations.util;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = getInstance();
+
+    private static SessionFactory getInstance() {
+        return new Configuration().configure().buildSessionFactory();
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return sessionFactory;
     }
 }
