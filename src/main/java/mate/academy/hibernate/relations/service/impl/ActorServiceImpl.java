b9d3a1739d7a010f088fad96c575dc36768764cc
ssx_corp <mr.dimov77@gmail.com>
2025-02-24 17:07:18 +0200

    Service classes created for Actor, Movie, Country

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,31 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
 public class ActorServiceImpl implements ActorService {
+    private final ActorDao actorDao;
+
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        if (actor != null) {
+            return actorDao.add(actor);
+        }
+        throw new RuntimeException("Cannot add null actor");
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        Optional<Actor> actor = actorDao.get(id);
+        if (actor.isPresent()) {
+            return actor.get();
+        }
+        throw new RuntimeException("Cannot find actor with id: " + id);
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,31 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        if (movie != null) {
+            return movieDao.add(movie);
+        }
+        throw new RuntimeException("Cannot add null movie");
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        Optional<Movie> movie = movieDao.get(id);
+        if (movie.isPresent()) {
+            return movie.get();
+        }
+        throw new RuntimeException("Cannot find movie with id: " + id);
     }
 }
