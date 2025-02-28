4a4fe62c3c64763dc7d998df2dcfccafd50429e3
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-02-28 20:55:37 +0200

    make dao model and service impl

--- src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/MovieDaoImpl.java
@@ -3,7 +3,10 @@ package mate.academy.hibernate.relations.dao.impl;
 import java.util.Optional;
 import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
+import mate.academy.hibernate.relations.util.DataProcessingException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MovieDaoImpl extends AbstractDao implements MovieDao {
     public MovieDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,36 @@ public class MovieDaoImpl extends AbstractDao implements MovieDao {
 
     @Override
     public Movie add(Movie movie) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = super.factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(movie);
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new DataProcessingException("Can't save an actor");
+        } finally {
+            session.close();
+        }
+
+        return movie;
     }
 
     @Override
     public Optional<Movie> get(Long id) {
-        return null;
+        Session session = null;
+        Optional<Movie> movie = null;
+        try {
+            session = super.factory.openSession();
+            movie = Optional.of(session.get(Movie.class, id));
+        } catch (Exception e) {
+            return Optional.empty();
+        } finally {
+            session.close();
+        }
+
+        return movie;
     }
 }
