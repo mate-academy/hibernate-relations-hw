ba36120e11b58d03e5d8cd862f850eb4d4fc16d8
Havrylenko Volodymyr <gvs88986@gmail.com>
2025-03-13 19:38:18 +0200

    homeTask_buildSuccess

---
+++ src/main/java/mate/academy/hibernate/relations/exception/DataProcessingException.java
@@ -0,0 +1,13 @@
+package mate.academy.hibernate.relations.exception;
+
+public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+}
