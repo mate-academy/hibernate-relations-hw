237ce4473709ce8369132443eb9a09687d8ce290
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-04-02 23:31:30 +0200

    Solution

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,22 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import lombok.AllArgsConstructor;
+import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
 
+@AllArgsConstructor
 public class CountryServiceImpl implements CountryService {
+
+    private final CountryDao countryDao;
+
     @Override
     public Country add(Country country) {
-        return null;
+        return countryDao.add(country);
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        return countryDao.get(id).get();
     }
 }
