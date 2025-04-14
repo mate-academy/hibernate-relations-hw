440dcc95329f8d8852bba40f6ccd55ad4cad9d1e
Olha <olha.oleshchuk@gmail.com>
2025-04-14 16:13:20 +0300

    all done - first try

--- src/main/java/mate/academy/hibernate/relations/Main.java
+++ src/main/java/mate/academy/hibernate/relations/Main.java
@@ -1,13 +1,6 @@
 package mate.academy.hibernate.relations;
 
-import java.util.ArrayList;
 import mate.academy.hibernate.relations.lib.Injector;
-import mate.academy.hibernate.relations.model.Actor;
-import mate.academy.hibernate.relations.model.Country;
-import mate.academy.hibernate.relations.model.Movie;
-import mate.academy.hibernate.relations.service.ActorService;
-import mate.academy.hibernate.relations.service.CountryService;
-import mate.academy.hibernate.relations.service.MovieService;
 import mate.academy.hibernate.relations.util.HibernateUtil;
 import org.hibernate.SessionFactory;
 
@@ -18,20 +11,6 @@ public class Main {
     public static void main(String[] args) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-        Country usa = new Country("USA");
-        CountryService countryService = (CountryService) injector.getInstance(CountryService.class);
-        countryService.add(usa);
-
-        Actor vinDiesel = new Actor("Vin Diesel");
-        vinDiesel.setCountry(usa);
-        ActorService actorService = (ActorService) injector.getInstance(ActorService.class);
-        actorService.add(vinDiesel);
-
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setActors(new ArrayList<>());
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
-        fastAndFurious.getActors().add(actorService.get(vinDiesel.getId()));
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
+        System.out.println("Hello world!");
     }
 }
