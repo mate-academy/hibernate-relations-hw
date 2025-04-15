679defbc7cea540460b41ac4046a4bd69e909119
Vitalii Kozovyi <vitalikkozovuy@gmail.com>
2025-04-15 17:28:47 +0200

    solution

--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -18,6 +18,6 @@ public class HibernateUtil {
     }
 
     public static SessionFactory getSessionFactory() {
-        return new Configuration().configure().buildSessionFactory();
+        return  sessionFactory;
     }
 }
