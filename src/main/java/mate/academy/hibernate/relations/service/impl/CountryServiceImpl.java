1abc131e8623c268541ab7906ceec9d05251d9c2
Viktoriia Dragneva <viktoriiadragneva@gmail.com>
2025-04-26 19:56:53 +0300

    SOLUTION

--- src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/CountryServiceImpl.java
@@ -1,16 +1,32 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.CountryDao;
+import mate.academy.hibernate.relations.dao.impl.CountryDaoImpl;
 import mate.academy.hibernate.relations.model.Country;
 import mate.academy.hibernate.relations.service.CountryService;
+import org.hibernate.SessionFactory;
 
 public class CountryServiceImpl implements CountryService {
+    private CountryDao countryDao;
+
+    public CountryServiceImpl(SessionFactory sessionFactory) {
+        this.countryDao = new CountryDaoImpl(sessionFactory);
+    }
+
     @Override
     public Country add(Country country) {
-        return null;
+        if (country == null) {
+            throw new IllegalArgumentException("Country can not be null!");
+        }
+        countryDao.add(country);
+        return country;
     }
 
     @Override
     public Country get(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException("ID can not be null!");
+        }
+        return countryDao.get(id).get();
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,32 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.dao.impl.MovieDaoImpl;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
+import org.hibernate.SessionFactory;
 
 public class MovieServiceImpl implements MovieService {
+    private MovieDao movieDao;
+
+    public MovieServiceImpl(SessionFactory sessionFactory) {
+        this.movieDao = new MovieDaoImpl(sessionFactory);
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        if (movie == null) {
+            throw new IllegalArgumentException("Actor can not be null!");
+        }
+        movieDao.add(movie);
+        return movie;
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        if (id == null) {
+            throw new IllegalArgumentException("Movie can not be null!");
+        }
+        return movieDao.get(id).get();
     }
 }
