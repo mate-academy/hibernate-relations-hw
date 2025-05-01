6dc08cafa1c055b4ffdfe95825dadef10d7ce0e0
kostya-savchenko <kostyantyn.savchenko@gmail.com>
2025-05-01 22:00:46 +0300

    added solution without errors in tests  with EntityNotFoundException() in get(Long id) v1.2

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -2,7 +2,6 @@ package mate.academy.hibernate.relations.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
 import mate.academy.hibernate.relations.dao.MovieDao;
-import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
