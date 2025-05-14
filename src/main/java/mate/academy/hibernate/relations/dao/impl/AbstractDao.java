6221df5cdeb3d81961c83d57f5a5814c54a78f7e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-05-14 08:57:59 +0300

    AbstractDao.java

--- src/main/java/mate/academy/hibernate/relations/dao/impl/AbstractDao.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/AbstractDao.java
@@ -1,11 +1,53 @@
 package mate.academy.hibernate.relations.dao.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
+import mate.academy.hibernate.relations.dao.exception.DataProcessingException;
+import mate.academy.hibernate.relations.model.Actor;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-public abstract class AbstractDao {
-    protected final SessionFactory factory;
+public class ActorDaoImpl extends AbstractDao implements ActorDao {
+    public ActorDaoImpl(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
 
-    protected AbstractDao(SessionFactory sessionFactory) {
-        this.factory = sessionFactory;
+    @Override
+    public Actor add(Actor actor) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = factory.openSession();
+            transaction = session.beginTransaction();
+            session.save(actor);
+            transaction.commit();
+            return actor;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add actor: " + actor + " to DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<Actor> get(Long id) {
+        Session session = null;
+        try {
+            session = factory.openSession();
+            Actor actor = session.get(Actor.class, id);
+            return Optional.ofNullable(actor);
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get actor from DB", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
