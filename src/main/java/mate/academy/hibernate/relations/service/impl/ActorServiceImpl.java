83d5e46110e9e79b9fc35ba7a9181b23264754a0
Ivanov <ivanshevchenko181@gmail.com>
2025-08-04 23:41:36 +0300

    Solved the task

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,26 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import java.util.Optional;
+import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
 
 public class ActorServiceImpl implements ActorService {
+    private ActorDao actorDao;
+
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        actorDao.add(actor);
+        return actor;
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        Optional<Actor> optionalActor = actorDao.get(id);
+        return optionalActor.orElse(null);
     }
 }
--- src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
+++ src/main/java/mate/academy/hibernate/relations/util/HibernateUtil.java
@@ -1,9 +1,28 @@
 package mate.academy.hibernate.relations.util;
 
+import mate.academy.hibernate.relations.model.Actor;
+import mate.academy.hibernate.relations.model.Country;
+import mate.academy.hibernate.relations.model.Movie;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = initSessionFactory();
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            Configuration configuration = new Configuration()
+                    .configure("hibernate.cfg.xml") // You must provide this XML in `resources`
+                    .addAnnotatedClass(Country.class)
+                    .addAnnotatedClass(Actor.class)
+                    .addAnnotatedClass(Movie.class);
+            return configuration.buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating SessionFactory", e);
+        }
+    }
+
     public static SessionFactory getSessionFactory() {
-        return null;
+        return sessionFactory;
     }
 }
