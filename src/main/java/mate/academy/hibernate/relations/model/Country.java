eedad213c4af52ea9aca1b1fec7c875aa1a6bd5e
Andrzej Pietkiewicz <pietan@me.com>
2025-08-05 14:16:05 +0200

    first solution

--- src/main/java/mate/academy/hibernate/relations/model/Country.java
+++ src/main/java/mate/academy/hibernate/relations/model/Country.java
@@ -1,8 +1,27 @@
 package mate.academy.hibernate.relations.model;
 
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "countries")
 public class Country implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "country", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Actor> actors;
 
     public Country() {
     }
