4579d33fc6489a696c9ca4484c920e66d03a0381
Andrii Honchar <andriihoncharw@gmail.com>
2025-09-05 13:01:40 +0300

    solution attemp #1

--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,30 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
+import mate.academy.hibernate.relations.exception.DataProcessingException;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
 
 public class MovieServiceImpl implements MovieService {
+    private MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        if (movie == null || movie.getTitle() == null) {
+            throw new DataProcessingException("Movie cannot be null");
+        }
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        return movieDao
+                .get(id)
+                .orElseThrow(
+                        () -> new DataProcessingException("Cannot find movie with id= " + id));
     }
 }
