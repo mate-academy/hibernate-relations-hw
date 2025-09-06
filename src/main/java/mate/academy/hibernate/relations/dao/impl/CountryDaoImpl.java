5caeadaee15a89977b277bc0553047958c8c1634
kozma <kozmakruckov@gmail.com>
2025-09-06 16:47:04 +0300

    added hibernate relations hw solution 1

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -1,9 +1,12 @@
 package mate.academy.hibernate.relations.dao.impl;
 
 import java.util.Optional;
+import mate.academy.hibernate.relations.DataProcessingException;
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class CountryDaoImpl extends AbstractDao implements CountryDao {
     public CountryDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +15,31 @@ public class CountryDaoImpl extends AbstractDao implements CountryDao {
 
     @Override
     public Country add(Country country) {
-        return null;
+        Session session = factory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(country);
+            transaction.commit();
+            return country;
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't insert country " + country, e);
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public Optional<Country> get(Long id) {
-        return null;
+        Session session = factory.openSession();
+        try {
+            return Optional.ofNullable(session.get(Country.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get country " + id, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
